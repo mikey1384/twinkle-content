5bac2c354564a438e0f93662fb9d70f79440c34e
mikey1384 <mikey1384@gmail.com>
2020-07-05 18:17:13 +0900

    more fixes

--- src/components/Comments/CommentInputArea.js
+++ src/components/Comments/CommentInputArea.js
@@ -1,20 +1,9 @@
-import React, { useState, useRef, useMemo } from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import InputForm from 'components/Forms/InputForm';
 import { useInputContext, useContentContext } from 'contexts';
-import { useMyState } from 'helpers/hooks';
-import Icon from '../Icon';
-import Attachment from 'components/Attachment';
-import Button from 'components/Button';
 import FileUploadStatusIndicator from 'components/FileUploadStatusIndicator';
 import { v1 as uuidv1 } from 'uuid';
-import { FILE_UPLOAD_XP_REQUIREMENT, mb } from 'constants/defaultValues';
-import AlertModal from 'components/Modals/AlertModal';
-import {
-  getFileInfoFromFileName,
-  addCommasToNumber
-} from 'helpers/stringHelpers';
-import FullTextReveal from 'components/Texts/FullTextReveal';
 
 CommentInputArea.propTypes = {
   autoFocus: PropTypes.bool,
@@ -55,11 +44,7 @@ export default function CommentInputArea({
     state: contentState,
     actions: { onSetCommentUploadingFile }
   } = useContentContext();
-  const { userId, profileTheme, authLevel, twinkleXP } = useMyState();
   const filePathRef = useRef(null);
-  const FileInputRef = useRef(null);
-  const [alertModalShown, setAlertModalShown] = useState(false);
-  const [onHover, setOnHover] = useState(false);
   const [commentContent, setCommentContent] = useState('');
   const contentType = targetCommentId ? 'comment' : parent.contentType;
   const contentId = targetCommentId || parent.contentId;
@@ -70,97 +55,23 @@ export default function CommentInputArea({
   const fileUploadProgress =
     contentState[contentType + contentId].fileUploadProgress;
 
-  const maxSize = useMemo(
-    () =>
-      authLevel > 3
-        ? 5000 * mb
-        : authLevel > 1
-        ? 3000 * mb
-        : authLevel === 1
-        ? 1000 * mb
-        : 300 * mb,
-    [authLevel]
-  );
-
-  const disabled = useMemo(
-    () => authLevel === 0 && twinkleXP < FILE_UPLOAD_XP_REQUIREMENT,
-    [authLevel, twinkleXP]
-  );
-
   return (
     <div style={{ ...style, position: 'relative' }} ref={InputFormRef}>
       {!uploadingFile && (
-        <div
-          style={{
-            display: 'flex',
-            width: '100%',
-            justifyContent: 'space-between'
-          }}
-        >
-          <InputForm
-            style={{
-              width: '100%'
-            }}
-            innerRef={innerRef}
-            clickListenerState={clickListenerState}
-            autoFocus={autoFocus}
-            onSubmit={handleSubmit}
-            parent={
-              subjectId
-                ? { contentId: subjectId, contentType: 'subject' }
-                : parent
-            }
-            rows={numInputRows}
-            placeholder={`Enter your ${inputTypeLabel} here...`}
-            targetCommentId={targetCommentId}
-          />
-          {attachment ? (
-            <Attachment
-              style={{ marginLeft: '1rem', fontSize: '1rem' }}
-              attachment={attachment}
-              onClose={() =>
-                onSetCommentAttachment({
-                  attachment: undefined,
-                  contentType,
-                  contentId
-                })
-              }
-            />
-          ) : (
-            <div>
-              {userId && (
-                <Button
-                  skeuomorphic
-                  color={profileTheme}
-                  onClick={() =>
-                    disabled ? null : FileInputRef.current.click()
-                  }
-                  onMouseEnter={() => setOnHover(true)}
-                  onMouseLeave={() => setOnHover(false)}
-                  style={{
-                    height: '4rem',
-                    width: '4rem',
-                    marginLeft: '1rem',
-                    opacity: disabled ? 0.2 : 1,
-                    cursor: disabled ? 'default' : 'pointer'
-                  }}
-                >
-                  <Icon size="lg" icon="upload" />
-                </Button>
-              )}
-              {userId && disabled && (
-                <FullTextReveal
-                  text={
-                    'Requires ' +
-                    addCommasToNumber(FILE_UPLOAD_XP_REQUIREMENT) +
-                    ' XP'
-                  }
-                  show={onHover}
-                />
-              )}
-            </div>
-          )}
-        </div>
+        <InputForm
+          innerRef={innerRef}
+          clickListenerState={clickListenerState}
+          autoFocus={autoFocus}
+          onSubmit={handleSubmit}
+          parent={
+            subjectId
+              ? { contentId: subjectId, contentType: 'subject' }
+              : parent
+          }
+          rows={numInputRows}
+          placeholder={`Enter your ${inputTypeLabel} here...`}
+          targetCommentId={targetCommentId}
+        />
       )}
       {uploadingFile && (
         <FileUploadStatusIndicator
@@ -169,21 +80,6 @@ export default function CommentInputArea({
           onFileUpload={handleFileUploadComplete}
           uploadComplete={fileUploadComplete}
           uploadProgress={fileUploadProgress}
-        />
-      )}
-      <input
-        ref={FileInputRef}
-        style={{ display: 'none' }}
-        type="file"
-        onChange={handleUpload}
-      />
-      {alertModalShown && (
-        <AlertModal
-          title="File is too large"
-          content={`The file size is larger than your limit of ${
-            maxSize / mb
-          } MB`}
-          onHide={() => setAlertModalShown(false)}
         />
       )}
     </div>
@@ -230,65 +126,5 @@ export default function CommentInputArea({
         contentId
       });
     }
-  }
-
-  function handleUpload(event) {
-    const fileObj = event.target.files[0];
-    if (fileObj.size / mb > maxSize) {
-      return setAlertModalShown(true);
-    }
-    const { fileType } = getFileInfoFromFileName(fileObj.name);
-    if (fileType === 'image') {
-      const reader = new FileReader();
-      reader.onload = (upload) => {
-        const payload = upload.target.result;
-        if (fileObj.name.split('.')[1] === 'gif') {
-          onSetCommentAttachment({
-            attachment: {
-              file: fileObj,
-              contentType: 'file',
-              fileType,
-              imageUrl: payload
-            },
-            contentType,
-            contentId
-          });
-        } else {
-          window.loadImage(
-            payload,
-            function (img) {
-              const imageUrl = img.toDataURL('image/jpeg');
-              const dataUri = imageUrl.replace(/^data:image\/\w+;base64,/, '');
-              const buffer = Buffer.from(dataUri, 'base64');
-              const file = new File([buffer], fileObj.name);
-
-              onSetCommentAttachment({
-                attachment: {
-                  file,
-                  contentType: 'file',
-                  fileType,
-                  imageUrl
-                },
-                contentType,
-                contentId
-              });
-            },
-            { orientation: true, canvas: true }
-          );
-        }
-      };
-      reader.readAsDataURL(fileObj);
-    } else {
-      onSetCommentAttachment({
-        attachment: {
-          file: fileObj,
-          contentType: 'file',
-          fileType
-        },
-        contentType,
-        contentId
-      });
-    }
-    event.target.value = null;
   }
 }
--- src/components/Forms/InputForm.js
+++ src/components/Forms/InputForm.js
@@ -2,13 +2,21 @@ import React, { useState, useMemo, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'components/Button';
 import Textarea from 'components/Texts/Textarea';
+import Icon from '../Icon';
+import Attachment from 'components/Attachment';
+import FullTextReveal from 'components/Texts/FullTextReveal';
+import AlertModal from 'components/Modals/AlertModal';
+import { FILE_UPLOAD_XP_REQUIREMENT, mb } from 'constants/defaultValues';
 import {
+  addCommasToNumber,
   exceedsCharLimit,
   stringIsEmpty,
   addEmoji,
-  finalizeEmoji
+  finalizeEmoji,
+  getFileInfoFromFileName
 } from 'helpers/stringHelpers';
 import { css } from 'emotion';
+import { useMyState } from 'helpers/hooks';
 import { useInputContext } from 'contexts';
 
 InputForm.propTypes = {
@@ -36,13 +44,28 @@ export default function InputForm({
   style = {},
   targetCommentId
 }) {
+  const { userId, profileTheme, authLevel, twinkleXP } = useMyState();
+  const maxSize = useMemo(
+    () =>
+      authLevel > 3
+        ? 5000 * mb
+        : authLevel > 1
+        ? 3000 * mb
+        : authLevel === 1
+        ? 1000 * mb
+        : 300 * mb,
+    [authLevel]
+  );
   const [submitting, setSubmitting] = useState(false);
+  const [alertModalShown, setAlertModalShown] = useState(false);
+  const FileInputRef = useRef(null);
   const {
     state,
-    actions: { onEnterComment }
+    actions: { onEnterComment, onSetCommentAttachment }
   } = useInputContext();
   const contentType = targetCommentId ? 'comment' : parent.contentType;
   const contentId = targetCommentId || parent.contentId;
+  const attachment = state[contentType + contentId]?.attachment;
   const prevText = useMemo(() => state[contentType + contentId]?.text || '', [
     contentId,
     contentType,
@@ -51,11 +74,14 @@ export default function InputForm({
   const textRef = useRef(prevText);
   const mounted = useRef(true);
   const [text, setText] = useState(prevText);
+  const [onHover, setOnHover] = useState(false);
+
   useEffect(() => {
     if (mounted.current) {
       handleSetText(prevText);
     }
   }, [prevText]);
+
   const commentExceedsCharLimit = useMemo(
     () =>
       exceedsCharLimit({
@@ -63,6 +89,11 @@ export default function InputForm({
         text
       }),
     [text]
+  );
+
+  const disabled = useMemo(
+    () => authLevel === 0 && twinkleXP < FILE_UPLOAD_XP_REQUIREMENT,
+    [authLevel, twinkleXP]
   );
 
   useEffect(() => {
@@ -78,51 +109,122 @@ export default function InputForm({
   }, []);
 
   return (
-    <div style={style} className={className}>
-      <div
-        style={{
-          position: 'relative',
-          ...formGroupStyle
-        }}
-      >
-        <Textarea
-          autoFocus={autoFocus}
-          innerRef={innerRef}
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
+        ...style
+      }}
+      className={className}
+    >
+      <div style={{ width: '100%' }}>
+        <div
           style={{
-            fontSize: '1.7rem',
-            ...(commentExceedsCharLimit?.style || {})
+            position: 'relative',
+            ...formGroupStyle
           }}
-          minRows={rows}
-          value={text}
-          placeholder={placeholder}
-          onChange={handleOnChange}
-          onKeyUp={handleKeyUp}
-        />
-        {commentExceedsCharLimit && (
-          <small style={{ color: 'red', fontSize: '1.3rem' }}>
-            {commentExceedsCharLimit.message}
-          </small>
+        >
+          <Textarea
+            autoFocus={autoFocus}
+            innerRef={innerRef}
+            style={{
+              fontSize: '1.7rem',
+              ...(commentExceedsCharLimit?.style || {})
+            }}
+            minRows={rows}
+            value={text}
+            placeholder={placeholder}
+            onChange={handleOnChange}
+            onKeyUp={handleKeyUp}
+          />
+          {commentExceedsCharLimit && (
+            <small style={{ color: 'red', fontSize: '1.3rem' }}>
+              {commentExceedsCharLimit.message}
+            </small>
+          )}
+        </div>
+        {(!stringIsEmpty(text) || attachment) && (
+          <div
+            className={css`
+              display: flex;
+              justify-content: flex-end;
+            `}
+          >
+            <Button
+              style={{ marginTop: '1rem', marginBottom: '0.5rem' }}
+              filled
+              color="green"
+              disabled={
+                submitting ||
+                (stringIsEmpty(text) && !attachment) ||
+                !!commentExceedsCharLimit
+              }
+              onClick={handleSubmit}
+            >
+              Tap This Button to Submit!
+            </Button>
+          </div>
         )}
       </div>
-      {!stringIsEmpty(text) && (
-        <div
-          className={css`
-            display: flex;
-            justify-content: flex-end;
-          `}
-        >
-          <Button
-            style={{ marginTop: '1rem', marginBottom: '0.5rem' }}
-            filled
-            color="green"
-            disabled={
-              submitting || stringIsEmpty(text) || !!commentExceedsCharLimit
-            }
-            onClick={handleSubmit}
-          >
-            Tap This Button to Submit!
-          </Button>
+      {attachment ? (
+        <Attachment
+          style={{ marginLeft: '1rem', fontSize: '1rem' }}
+          attachment={attachment}
+          onClose={() =>
+            onSetCommentAttachment({
+              attachment: undefined,
+              contentType,
+              contentId
+            })
+          }
+        />
+      ) : (
+        <div>
+          {userId && (
+            <Button
+              skeuomorphic
+              color={profileTheme}
+              onClick={() => (disabled ? null : FileInputRef.current.click())}
+              onMouseEnter={() => setOnHover(true)}
+              onMouseLeave={() => setOnHover(false)}
+              style={{
+                height: '4rem',
+                width: '4rem',
+                marginLeft: '1rem',
+                opacity: disabled ? 0.2 : 1,
+                cursor: disabled ? 'default' : 'pointer'
+              }}
+            >
+              <Icon size="lg" icon="upload" />
+            </Button>
+          )}
+          {userId && disabled && (
+            <FullTextReveal
+              text={
+                'Requires ' +
+                addCommasToNumber(FILE_UPLOAD_XP_REQUIREMENT) +
+                ' XP'
+              }
+              show={onHover}
+            />
+          )}
         </div>
+      )}
+      <input
+        ref={FileInputRef}
+        style={{ display: 'none' }}
+        type="file"
+        onChange={handleUpload}
+      />
+      {alertModalShown && (
+        <AlertModal
+          title="File is too large"
+          content={`The file size is larger than your limit of ${
+            maxSize / mb
+          } MB`}
+          onHide={() => setAlertModalShown(false)}
+        />
       )}
     </div>
   );
@@ -154,5 +256,65 @@ export default function InputForm({
       setSubmitting(false);
       console.error(error);
     }
+  }
+
+  function handleUpload(event) {
+    const fileObj = event.target.files[0];
+    if (fileObj.size / mb > maxSize) {
+      return setAlertModalShown(true);
+    }
+    const { fileType } = getFileInfoFromFileName(fileObj.name);
+    if (fileType === 'image') {
+      const reader = new FileReader();
+      reader.onload = (upload) => {
+        const payload = upload.target.result;
+        if (fileObj.name.split('.')[1] === 'gif') {
+          onSetCommentAttachment({
+            attachment: {
+              file: fileObj,
+              contentType: 'file',
+              fileType,
+              imageUrl: payload
+            },
+            contentType,
+            contentId
+          });
+        } else {
+          window.loadImage(
+            payload,
+            function (img) {
+              const imageUrl = img.toDataURL('image/jpeg');
+              const dataUri = imageUrl.replace(/^data:image\/\w+;base64,/, '');
+              const buffer = Buffer.from(dataUri, 'base64');
+              const file = new File([buffer], fileObj.name);
+
+              onSetCommentAttachment({
+                attachment: {
+                  file,
+                  contentType: 'file',
+                  fileType,
+                  imageUrl
+                },
+                contentType,
+                contentId
+              });
+            },
+            { orientation: true, canvas: true }
+          );
+        }
+      };
+      reader.readAsDataURL(fileObj);
+    } else {
+      onSetCommentAttachment({
+        attachment: {
+          file: fileObj,
+          contentType: 'file',
+          fileType
+        },
+        contentType,
+        contentId
+      });
+    }
+    event.target.value = null;
   }
 }
