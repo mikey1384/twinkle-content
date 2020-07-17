fcf07ed98b91961efb9b0696267909a624ab1147
mikey1384 <mikey1384@gmail.com>
2020-07-17 21:11:47 +0900

    made by user markable for urls

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,4 +1,4 @@
-export const clientVersion = '1.3.94';
+export const clientVersion = '1.3.95';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
