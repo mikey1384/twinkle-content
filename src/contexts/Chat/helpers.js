b2ffde1ca20bf01f399e351580678c6ee5a3fd9a
mikey1384 <mikey1384@gmail.com>
2020-07-27 20:40:34 +0900

    more refactoring

---
+++ src/contexts/Chat/helpers.js
@@ -0,0 +1,19 @@
+export function determineSelectedChatTab({
+  currentChatType,
+  currentSelectedChatTab,
+  selectedChatTab,
+  selectedChannel
+}) {
+  let newSelectedChatTab = currentSelectedChatTab;
+  if (selectedChatTab) {
+    newSelectedChatTab = selectedChatTab;
+  } else if (currentSelectedChatTab === 'class' && !selectedChannel?.isClass) {
+    newSelectedChatTab = 'home';
+  }
+
+  return {
+    selectedChatTab: newSelectedChatTab,
+    chatType:
+      selectedChatTab !== 'home' || selectedChannel ? null : currentChatType
+  };
+}
