260feb643961039be5317e416ff3bd2df3eabfd2
mikey1384 <mikey1384@gmail.com>
2020-07-27 19:52:59 +0900

    minor fix

--- src/containers/Chat/Body/MessagesContainer/ChannelHeader/index.js
+++ src/containers/Chat/Body/MessagesContainer/ChannelHeader/index.js
@@ -323,6 +323,7 @@ export default function ChannelHeader({
               }}
               onReloadChatSubject={handleReloadChatSubject}
               searchResults={subjectSearchResults}
+              theme={theme}
             />
           )}
         </>
