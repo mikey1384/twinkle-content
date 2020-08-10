98e290d0483205f2ab9ff58453048e9001d8ab06
mikey1384 <mikey1384@gmail.com>
2020-08-10 18:51:51 +0900

    more shop updates

--- src/containers/Home/Store/index.js
+++ src/containers/Home/Store/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icon from 'components/Icon';
 import { css } from 'emotion';
 import { addCommasToNumber } from 'helpers/stringHelpers';
 import { borderRadius, Color } from 'constants/css';
@@ -16,10 +17,33 @@ export default function Store() {
       >
         <p
           className={css`
-            font-size: 2rem;
+            font-weight: bold;
+            font-size: 2.3rem;
           `}
         >
           Your Karma Points: {addCommasToNumber(1000)}
+        </p>
+      </div>
+      <div
+        className={css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 1rem;
+          font-size: 2rem;
+          background: #fff;
+          padding: 1rem;
+          border: 1px solid ${Color.borderGray()};
+          border-radius: ${borderRadius};
+        `}
+      >
+        <Icon
+          style={{ fontSize: '8rem', marginTop: '1rem' }}
+          icon="shopping-bag"
+        />
+        <p style={{ marginTop: '2rem' }}>
+          Twinkle Store is currently under construction and will become
+          available later this year
         </p>
       </div>
     </div>
