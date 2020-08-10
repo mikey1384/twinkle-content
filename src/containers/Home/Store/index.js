a9a3fcc6d60d2adf5153ddf13a954c5b2f941c5b
mikey1384 <mikey1384@gmail.com>
2020-08-10 17:48:41 +0900

    minor ifx

--- src/containers/Home/Store/index.js
+++ src/containers/Home/Store/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { css } from 'emotion';
+import { addCommasToNumber } from 'helpers/stringHelpers';
+import { borderRadius, Color } from 'constants/css';
 
 export default function Store() {
   return (
     <div>
-      <div>Store</div>
+      <div
+        className={css`
+          background: #fff;
+          padding: 1rem;
+          border: 1px solid ${Color.borderGray()};
+          border-radius: ${borderRadius};
+        `}
+      >
+        <p
+          className={css`
+            font-size: 2rem;
+          `}
+        >
+          Your Karma Points: {addCommasToNumber(1000)}
+        </p>
+      </div>
     </div>
   );
 }
