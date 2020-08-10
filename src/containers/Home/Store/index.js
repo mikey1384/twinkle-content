84bd006959d2e1b19994684a1e795be7ea6bdaa7
mikey1384 <mikey1384@gmail.com>
2020-08-10 20:27:39 +0900

    add karma points instructions

--- src/containers/Home/Store/index.js
+++ src/containers/Home/Store/index.js
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Icon from 'components/Icon';
 import { css } from 'emotion';
 import { addCommasToNumber } from 'helpers/stringHelpers';
 import { borderRadius, Color, mobileMaxWidth } from 'constants/css';
+import { useMyState } from 'helpers/hooks';
 
 export default function Store() {
+  const { authLevel, userType, userId } = useMyState();
+
+  const instructionText = useMemo(() => {
+    if (!authLevel || authLevel < 2) {
+      return (
+        <span>
+          Your Karma Points = Total number of <b>Twinkles</b> you rewarded + (3
+          × Total number of <b>Twinkle Coins</b> you earned{' '}
+          <b>for recommending posts</b>)
+        </span>
+      );
+    }
+    return 'Your Karma Points = Total number of posts you rewarded × 5';
+  }, [authLevel]);
+
   return (
     <div>
-      <div
-        className={css`
-          background: #fff;
-          padding: 1rem;
-          border: 1px solid ${Color.borderGray()};
-          border-radius: ${borderRadius};
-          @media (max-width: ${mobileMaxWidth}) {
-            border-radius: 0;
-            border-top: 0;
-            border-left: 0;
-            border-right: 0;
-          }
-        `}
-      >
-        <p
+      {userId && (
+        <div
           className={css`
-            font-weight: bold;
-            font-size: 2.3rem;
+            background: #fff;
+            padding: 1rem;
+            border: 1px solid ${Color.borderGray()};
+            border-radius: ${borderRadius};
+            @media (max-width: ${mobileMaxWidth}) {
+              border-radius: 0;
+              border-top: 0;
+              border-left: 0;
+              border-right: 0;
+            }
           `}
         >
-          Your Karma Points: {addCommasToNumber(1000)}
-        </p>
-      </div>
+          <p
+            className={css`
+              font-weight: bold;
+              font-size: 2.3rem;
+            `}
+          >
+            You have {addCommasToNumber(1000)} Karma Points
+          </p>
+          <div
+            className={css`
+              margin-top: 2rem;
+            `}
+          >
+            {userType && (
+              <p
+                className={css`
+                  font-size: 2rem;
+                  font-weight: bold;
+                `}
+              >
+                {userType}
+              </p>
+            )}
+            <p
+              className={css`
+                font-size: 1.7rem;
+              `}
+            >
+              {instructionText}
+            </p>
+          </div>
+        </div>
+      )}
       <div
         className={css`
           display: flex;
