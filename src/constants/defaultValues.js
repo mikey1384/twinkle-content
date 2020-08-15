32003a2790f707f7e7675737158f2ec1a2ce17b1
mikey1384 <mikey1384@gmail.com>
2020-08-15 14:20:52 +0900

    bug fix

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,4 +1,4 @@
-export const clientVersion = '1.4.23';
+export const clientVersion = '1.4.24';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
--- src/containers/Chat/Body/MessagesContainer/index.js
+++ src/containers/Chat/Body/MessagesContainer/index.js
@@ -873,6 +873,23 @@ export default function MessagesContainer({
       });
     }
     setInviteUsersModalShown(false);
+    if (!isClass) {
+      onSubmitMessage({
+        message: {
+          channelId: selectedChannelId,
+          userId,
+          username,
+          id: uuidv1(),
+          profilePicId,
+          content: `sent ${users.length === 1 ? 'an ' : ''}invitation message${
+            users.length > 1 ? 's' : ''
+          } to ${
+            users.length > 1 ? `${users.length} users` : users[0].username
+          }`,
+          isNotification: true
+        }
+      });
+    }
   }
 
   function handleLeaveConfirm() {
