253c06b20bbae0f19f6038c51adad60b00ed12e8
mikey1384 <mikey1384@gmail.com>
2020-08-19 22:43:06 +0900

    more updates

--- src/containers/Tasks/TaskList/index.js
+++ src/containers/Tasks/TaskList/index.js
@@ -24,8 +24,28 @@ export default function TaskList({ style, className }) {
               </div>
             </div>
           </ListItem>
-          <ListItem style={{ marginTop: '1rem' }}>Item two</ListItem>
-          <ListItem style={{ marginTop: '1rem' }}>Item three</ListItem>
+          <ListItem style={{ marginTop: '1rem' }}>
+            <p style={{ fontSize: '2rem', fontWeight: 'bold' }}>
+              Take a Screenshot
+            </p>
+            <div style={{ marginTop: '1rem', display: 'flex' }}>
+              <img src={Screenshot} style={{ width: '10rem' }} />
+              <div style={{ marginLeft: '1rem', fontSize: '1.7rem' }}>
+                {`Take a picture of your computer screen for XP! Don't worry if you don't know how to do this - we will teach you!`}
+              </div>
+            </div>
+          </ListItem>
+          <ListItem style={{ marginTop: '1rem' }}>
+            <p style={{ fontSize: '2rem', fontWeight: 'bold' }}>
+              Take a Screenshot
+            </p>
+            <div style={{ marginTop: '1rem', display: 'flex' }}>
+              <img src={Screenshot} style={{ width: '10rem' }} />
+              <div style={{ marginLeft: '1rem', fontSize: '1.7rem' }}>
+                {`Take a picture of your computer screen for XP! Don't worry if you don't know how to do this - we will teach you!`}
+              </div>
+            </div>
+          </ListItem>
         </div>
       </div>
     </div>
