75d2cbc3b02b69b566c546c59169bd68ea6f99e7
mikey1384 <mikey1384@gmail.com>
2020-08-20 22:38:36 +0900

    more updates

--- src/containers/Tasks/TaskList/index.js
+++ src/containers/Tasks/TaskList/index.js
@@ -15,23 +15,23 @@ export default function TaskList({ style, className }) {
         <div style={{ marginTop: '1rem' }}>
           <ListItem>
             <p style={{ fontSize: '2rem', fontWeight: 'bold' }}>
-              Take a Screenshot
-            </p>
-            <div style={{ marginTop: '1rem', display: 'flex' }}>
-              <img src={Screenshot} style={{ width: '10rem' }} />
-              <div style={{ marginLeft: '1rem', fontSize: '1.7rem' }}>
-                {`Take a picture of your computer screen!`}
-              </div>
-            </div>
-          </ListItem>
-          <ListItem style={{ marginTop: '1rem' }}>
-            <p style={{ fontSize: '2rem', fontWeight: 'bold' }}>
               Copy and Paste
             </p>
             <div style={{ marginTop: '1rem', display: 'flex' }}>
               <img src={Screenshot} style={{ width: '10rem' }} />
               <div style={{ marginLeft: '1rem', fontSize: '1.7rem' }}>
                 {`Copy a block of text and paste it somewhere else!`}
+              </div>
+            </div>
+          </ListItem>
+          <ListItem style={{ marginTop: '1rem' }}>
+            <p style={{ fontSize: '2rem', fontWeight: 'bold' }}>
+              Take a Screenshot
+            </p>
+            <div style={{ marginTop: '1rem', display: 'flex' }}>
+              <img src={Screenshot} style={{ width: '10rem' }} />
+              <div style={{ marginLeft: '1rem', fontSize: '1.7rem' }}>
+                {`Take a picture of your computer screen!`}
               </div>
             </div>
           </ListItem>
