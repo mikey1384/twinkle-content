dda532f55184f86e79195a336f19348079857cb9
mikey1384 <mikey1384@gmail.com>
2020-08-24 16:34:11 +0900

    wire task request helper

--- src/contexts/requestHelpers/index.js
+++ src/contexts/requestHelpers/index.js
@@ -1,6 +1,7 @@
 import chatRequestHelpers from './chat';
 import contentRequestHelpers from './content';
 import notificationRequestHelpers from './notification';
+import taskRequestHelpers from './task';
 import userRequestHelpers from './user';
 
 const token = () =>
@@ -17,6 +18,7 @@ export default function requestHelpers(handleError) {
     auth,
     ...contentRequestHelpers({ auth, handleError }),
     ...notificationRequestHelpers({ auth, handleError }),
+    ...taskRequestHelpers({ auth, handleError }),
     ...userRequestHelpers({ auth, handleError, token }),
     ...chatRequestHelpers({ auth, handleError })
   };
