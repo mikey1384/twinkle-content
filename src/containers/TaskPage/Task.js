4fa099b7f486b660428e00ff70c417a2c26c4a68
mikey1384 <mikey1384@gmail.com>
2020-08-25 21:15:18 +0900

    more task page updates

--- src/containers/TaskPage/Task.js
+++ src/containers/TaskPage/Task.js
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from 'emotion';
 import { Color, borderRadius } from 'constants/css';
+import { gifTable } from 'constants/defaultValues';
 
 Task.propTypes = {
   title: PropTypes.string,
   subtitle: PropTypes.string,
   description: PropTypes.string,
-  style: PropTypes.object
+  style: PropTypes.object,
+  taskId: PropTypes.number
 };
-export default function Task({ title, subtitle, description, style }) {
+export default function Task({ title, subtitle, description, style, taskId }) {
   return (
     <div
       className={css`
@@ -19,8 +21,15 @@ export default function Task({ title, subtitle, description, style }) {
       `}
       style={style}
     >
-      <h1>{title}</h1>
-      <p style={{ fontSize: '1.7rem' }}>{subtitle}</p>
+      <div>
+        <h1>{title}</h1>
+        <p style={{ fontSize: '1.7rem' }}>{subtitle}</p>
+      </div>
+      <div
+        style={{ marginTop: '3rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <img src={gifTable[taskId]} />
+      </div>
       {description && <div>{description}</div>}
     </div>
   );
