47e3061426681c80313454d9767f35fad2cc0074
mikey1384 <mikey1384@gmail.com>
2020-08-25 20:01:46 +0900

    more task page update

--- src/containers/TaskPage/Task.js
+++ src/containers/TaskPage/Task.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from 'emotion';
+import { Color, borderRadius } from 'constants/css';
 
 Task.propTypes = {
   taskId: PropTypes.number,
   title: PropTypes.string,
   subtitle: PropTypes.string,
-  description: PropTypes.string
+  description: PropTypes.string,
+  style: PropTypes.object
 };
-export default function Task({ taskId, title, subtitle, description }) {
+export default function Task({ taskId, title, subtitle, description, style }) {
   return (
-    <div>
+    <div
+      className={css`
+        border: 1px solid ${Color.borderGray()};
+        border-radius: ${borderRadius};
+      `}
+      style={style}
+    >
       <div>{title}</div>
       <div>{subtitle}</div>
       <div>{taskId}</div>
