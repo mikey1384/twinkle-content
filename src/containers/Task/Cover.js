fc6ea195e174ba6ebd42fc0da7c029c135a5d933
mikey1384 <mikey1384@gmail.com>
2020-09-08 19:08:12 +0900

    more updates

--- src/containers/Task/Cover.js
+++ src/containers/Task/Cover.js
@@ -3,40 +3,60 @@ import { useMyState } from 'helpers/hooks';
 import { css } from 'emotion';
 import { Color } from 'constants/css';
 import ProfilePic from 'components/ProfilePic';
+import Button from 'components/Button';
 
 export default function Cover() {
-  const { profileTheme, profilePicId, userId, username } = useMyState();
+  const {
+    profileTheme,
+    profilePicId,
+    userId,
+    username,
+    canEdit
+  } = useMyState();
 
   return (
     <div
       className={css`
         height: 15vh;
         display: flex;
-        align-items: center;
+        justify-content: space-between;
         background: ${Color[profileTheme]()};
+        padding: 0 5%;
       `}
     >
-      <ProfilePic
-        className={css`
-          margin-left: 5%;
-          width: 9rem;
-          height: 9rem;
-          font-size: 2rem;
-          z-index: 10;
-        `}
-        userId={userId}
-        profilePicId={profilePicId}
-      />
-      <div
-        className={css`
-          margin-left: 3rem;
-          font-size: 3rem;
-          color: #fff;
-          font-weight: bold;
-        `}
-      >
-        {username}
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <ProfilePic
+          className={css`
+            width: 9rem;
+            height: 9rem;
+            font-size: 2rem;
+            z-index: 10;
+          `}
+          userId={userId}
+          profilePicId={profilePicId}
+        />
+        <div
+          className={css`
+            margin-left: 3rem;
+            font-size: 3rem;
+            color: #fff;
+            font-weight: bold;
+          `}
+        >
+          {username}
+        </div>
       </div>
+      {canEdit && (
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'flex-end',
+            paddingBottom: '1rem'
+          }}
+        >
+          <Button skeuomorphic>Add Tutorial</Button>
+        </div>
+      )}
     </div>
   );
 }
