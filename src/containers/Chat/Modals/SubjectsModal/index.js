30029c8a6e1e0e34d1e8c6682d1bbc44ae95a0ac
mikey1384 <mikey1384@gmail.com>
2020-09-12 08:04:46 +0900

    bug fix

--- src/containers/Chat/Modals/SubjectsModal/index.js
+++ src/containers/Chat/Modals/SubjectsModal/index.js
@@ -2,17 +2,12 @@ import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'components/Modal';
 import Button from 'components/Button';
-import request from 'axios';
 import LoadMoreButton from 'components/Buttons/LoadMoreButton';
 import SubjectItem from './SubjectItem';
 import Loading from 'components/Loading';
 import ConfirmModal from 'components/Modals/ConfirmModal';
 import { Color } from 'constants/css';
 import { useAppContext } from 'contexts';
-import { useMyState } from 'helpers/hooks';
-import URL from 'constants/URL';
-
-const API_URL = `${URL}/chat`;
 
 SubjectsModal.propTypes = {
   channelId: PropTypes.number.isRequired,
@@ -30,9 +25,8 @@ export default function SubjectsModal({
   userIsOwner
 }) {
   const {
-    requestHelpers: { deleteChatSubject, loadMoreSubjects }
+    requestHelpers: { deleteChatSubject, loadSubjects, loadMoreSubjects }
   } = useAppContext();
-  const { userId } = useMyState();
   const [deleteTarget, setDeleteTarget] = useState(null);
   const [loaded, setLoaded] = useState(false);
   const [mySubjects, setMySubjects] = useState({
@@ -49,14 +43,10 @@ export default function SubjectsModal({
 
   useEffect(() => {
     mounted.current = true;
-    loadSubjects();
-    async function loadSubjects() {
+    handleLoadSubjects();
+    async function handleLoadSubjects() {
       try {
-        const {
-          data: { mySubjects, allSubjects }
-        } = await request.get(
-          `${API_URL}/chatSubject/modal?userId=${userId}&channelId=${channelId}`
-        );
+        const { mySubjects, allSubjects } = await loadSubjects({ channelId });
         if (mounted.current) {
           setMySubjects(mySubjects);
           setAllSubjects(allSubjects);
@@ -190,7 +180,8 @@ export default function SubjectsModal({
     const lastSubject = targetSubjects[targetSubjects.length - 1];
     const { subjects, loadMoreButton } = await loadMoreSubjects({
       mineOnly,
-      lastSubject
+      lastSubject,
+      channelId
     });
     if (mineOnly) {
       setMySubjects({
