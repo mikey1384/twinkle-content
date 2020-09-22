cbdc987315b2c650e4386b92d661af830f9ae396
mikey1384 <mikey1384@gmail.com>
2020-09-23 07:45:30 +0900

    more updates

--- src/components/InteractiveContent/AddSlide.js
+++ src/components/InteractiveContent/AddSlide.js
@@ -1,41 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'components/Button';
 import Icon from 'components/Icon';
 import { borderRadius, Color } from 'constants/css';
-import NewSlideForm from './NewSlideForm';
 
 AddSlide.propTypes = {
   style: PropTypes.object
 };
 
 export default function AddSlide({ style }) {
-  const [isAddingSlide, setIsAddingSlide] = useState(false);
   return (
     <div style={{ width: '100%', ...style }}>
-      {isAddingSlide ? (
-        <NewSlideForm />
-      ) : (
-        <div
-          style={{
-            background: '#fff',
-            borderRadius,
-            padding: '1.5rem',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%',
-            border: `1px solid ${Color.borderGray()}`
-          }}
-        >
-          <div style={{ display: 'flex' }}>
-            <Button onClick={() => setIsAddingSlide(true)} skeuomorphic>
-              <Icon icon="plus" />
-              <span style={{ marginLeft: '0.7rem' }}>Add a Slide</span>
-            </Button>
-          </div>
+      <div
+        style={{
+          background: '#fff',
+          borderRadius,
+          padding: '1.5rem',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          border: `1px solid ${Color.borderGray()}`
+        }}
+      >
+        <div style={{ display: 'flex' }}>
+          <Button onClick={() => console.log('adding new slide')} skeuomorphic>
+            <Icon icon="plus" />
+            <span style={{ marginLeft: '0.7rem' }}>Add a Slide</span>
+          </Button>
         </div>
-      )}
+      </div>
       <div
         style={{
           background: '#fff',
