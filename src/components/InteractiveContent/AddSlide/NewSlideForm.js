810f710d19acae0739b2235daf14ece0fe4e6436
mikey1384 <mikey1384@gmail.com>
2020-09-22 22:52:58 +0900

    more updates

--- src/components/InteractiveContent/AddSlide/NewSlideForm.js
+++ src/components/InteractiveContent/AddSlide/NewSlideForm.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { css } from 'emotion';
+import { Color, borderRadius } from 'constants/css';
 
 export default function NewSlideForm() {
   return (
-    <div>
+    <div
+      className={css`
+        background: #fff;
+        width: 100%;
+        border: 1px solid ${Color.borderGray()};
+        border-radius: ${borderRadius};
+      `}
+    >
       <div>Adding new slide!!</div>
     </div>
   );
