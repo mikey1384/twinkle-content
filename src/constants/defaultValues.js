b23e095af988d8b37d7f817171f8d05d7c88cb4d
mikey1384 <mikey1384@gmail.com>
2020-09-26 08:49:25 +0900

    improve video player performance

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,4 +1,4 @@
-export const clientVersion = '1.4.38';
+export const clientVersion = '1.4.39';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
