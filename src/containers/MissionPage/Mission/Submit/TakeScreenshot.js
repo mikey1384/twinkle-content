aa37d0373e68bb838d8a7b78f72395c57d299069
mikey1384 <mikey1384@gmail.com>
2020-10-06 16:40:42 +0900

    minor fix

--- src/containers/MissionPage/Mission/Submit/TakeScreenshot.js
+++ src/containers/MissionPage/Mission/Submit/TakeScreenshot.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useRef, useMemo, useState } from 'react';
 import Button from 'components/Button';
+import AlertModal from 'components/Modals/AlertModal';
+import { mb } from 'constants/defaultValues';
+import { useMyState } from 'helpers/hooks';
+import { getFileInfoFromFileName } from 'helpers/stringHelpers';
 
 export default function TakeScreenshot() {
+  const { authLevel } = useMyState();
+  const [alertModalShown, setAlertModalShown] = useState(false);
+  const FileInputRef = useRef(null);
+  const maxSize = useMemo(
+    () =>
+      authLevel > 3
+        ? 5000 * mb
+        : authLevel > 1
+        ? 3000 * mb
+        : authLevel === 1
+        ? 1000 * mb
+        : 300 * mb,
+    [authLevel]
+  );
+
   return (
     <div
       style={{ display: 'flex', justifyContent: 'center', marginTop: '5rem' }}
@@ -10,10 +29,54 @@ export default function TakeScreenshot() {
         color="darkBlue"
         skeuomorphic
         style={{ fontSize: '2rem' }}
-        onClick={() => console.log('clicked')}
+        onClick={() => FileInputRef.current.click()}
       >
         Submit Screenshot
       </Button>
+      <input
+        ref={FileInputRef}
+        style={{ display: 'none' }}
+        type="image"
+        onChange={handleFileSelection}
+      />
+      {alertModalShown && (
+        <AlertModal
+          title="File is too large"
+          content={`The file size is larger than your limit of ${
+            maxSize / mb
+          } MB`}
+          onHide={() => setAlertModalShown(false)}
+        />
+      )}
     </div>
   );
+
+  function handleFileSelection(event) {
+    const fileObj = event.target.files[0];
+    if (fileObj.size / mb > maxSize) {
+      return setAlertModalShown(true);
+    }
+    const { fileType } = getFileInfoFromFileName(fileObj.name);
+    if (fileType === 'image') {
+      const reader = new FileReader();
+      reader.onload = (upload) => {
+        const payload = upload.target.result;
+        window.loadImage(
+          payload,
+          function (img) {
+            const imageUri = img.toDataURL('image/jpeg');
+            const dataUri = imageUri.replace(/^data:image\/\w+;base64,/, '');
+            const buffer = Buffer.from(dataUri, 'base64');
+            const file = new File([buffer], fileObj.name);
+            console.log(fileType, file, imageUri);
+          },
+          { orientation: true, canvas: true }
+        );
+      };
+      reader.readAsDataURL(fileObj);
+    } else {
+      // return error
+    }
+    event.target.value = null;
+  }
 }
