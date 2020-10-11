ede1298c0d8ab280121fa49debf472be3ca1d86d
mikey1384 <mikey1384@gmail.com>
2020-10-11 15:18:19 +0900

    add copy and paste component

---
+++ src/containers/MissionPage/Main/Mission/Submit/CopyAndPaste.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function CopyAndPaste() {
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus
+        commodo purus eget tempus. In suscipit euismod ex, sit amet maximus sem
+        egestas ac. Duis libero massa, imperdiet a neque et, posuere aliquam
+        metus. Curabitur rhoncus semper augue, sit amet placerat libero mattis
+        eu. Donec id nulla venenatis, eleifend enim quis, placerat est. Nulla
+        facilisi. Nunc molestie dolor sed odio dignissim cursus et eu ligula.
+        Suspendisse feugiat iaculis turpis, id dictum massa finibus eu. Cras
+        tempus sagittis commodo. Nunc consectetur ut mi vel pharetra. Integer
+        posuere diam at nulla porttitor suscipit. Aliquam eget ligula non turpis
+        ultrices pulvinar in in mi. Sed fermentum libero sed nisl feugiat
+        rhoncus. Etiam fringilla porta feugiat. Donec et arcu venenatis, pretium
+        nulla ut, convallis odio. Duis lectus libero, venenatis vitae sodales
+        eget, iaculis nec mi. Nulla porttitor commodo convallis. Aenean vel orci
+        tristique felis lobortis hendrerit ut eleifend turpis. Fusce posuere,
+        lectus eu ullamcorper viverra, lorem orci finibus magna, quis varius leo
+        tortor in nulla. Nunc tincidunt porttitor mauris ut fermentum.
+        Suspendisse potenti. Fusce vel dui ut lorem pulvinar porttitor at id
+        felis. Nullam et malesuada leo, nec congue ipsum. Nulla vehicula libero
+        in nulla aliquet mollis.
+      </div>
+    </div>
+  );
+}
