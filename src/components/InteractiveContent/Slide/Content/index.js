de4d78335bb7ab1ce1346dac8fe3dc6e9945ab3f
mikey1384 <mikey1384@gmail.com>
2020-10-29 23:50:16 +0900

    minor fix

--- src/components/InteractiveContent/Slide/Content/index.js
+++ src/components/InteractiveContent/Slide/Content/index.js
@@ -91,6 +91,7 @@ export default function Content({
       {attachment && (
         <div
           style={{
+            marginTop: '1rem',
             width: '100%',
             height: '100%',
             display: 'flex',
