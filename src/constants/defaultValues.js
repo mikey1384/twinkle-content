c9a911947c08a6eb583981fe84293ece5b30d11b
mikey1384 <mikey1384@gmail.com>
2020-10-30 12:02:18 +0900

    missions feature added

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,4 +1,4 @@
-export const clientVersion = '1.4.45';
+export const clientVersion = '1.4.46';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
