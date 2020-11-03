7f9a1b7c3d9de80e7dca134e85e8dc98cfa1c764
mikey1384 <mikey1384@gmail.com>
2020-11-03 16:13:39 +0900

    more fix

---
+++ src/containers/Home/Store/KarmaStatus.js
@@ -0,0 +1,191 @@
+import React, { useEffect, useRef, useMemo, useState } from 'react';
+import { css } from 'emotion';
+import { borderRadius, Color, mobileMaxWidth } from 'constants/css';
+import { addCommasToNumber } from 'helpers/stringHelpers';
+import { useMyState } from 'helpers/hooks';
+import { useAppContext } from 'contexts';
+import Loading from 'components/Loading';
+
+export default function KarmaStatus() {
+  const recommendationsMultiplier = 10;
+  const postsMultiplier = 3;
+  const {
+    requestHelpers: { loadKarmaPoints }
+  } = useAppContext();
+  const mounted = useRef(true);
+  const { authLevel, userType, userId } = useMyState();
+  const [loadingKarma, setLoadingKarma] = useState(false);
+  const [karmaPoints, setKarmaPoints] = useState(0);
+  const [numTwinklesRewarded, setNumTwinklesRewarded] = useState(0);
+  const [numApprovedRecommendations, setNumApprovedRecommendations] = useState(
+    0
+  );
+  const [numPostsRewarded, setNumPostsRewarded] = useState(0);
+  useEffect(() => {
+    return function cleanUp() {
+      mounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    if (userId) {
+      handleLoadKarmaPoints();
+    }
+
+    async function handleLoadKarmaPoints() {
+      if (mounted.current) {
+        setLoadingKarma(true);
+      }
+      const {
+        numTwinklesRewarded,
+        numApprovedRecommendations,
+        numPostsRewarded
+      } = await loadKarmaPoints();
+
+      if (authLevel < 2) {
+        if (mounted.current) {
+          setKarmaPoints(
+            numTwinklesRewarded +
+              recommendationsMultiplier * numApprovedRecommendations
+          );
+        }
+        if (mounted.current) {
+          setNumTwinklesRewarded(numTwinklesRewarded);
+        }
+        if (mounted.current) {
+          setNumApprovedRecommendations(numApprovedRecommendations);
+        }
+      } else {
+        if (mounted.current) {
+          setKarmaPoints(postsMultiplier * numPostsRewarded);
+        }
+        if (mounted.current) {
+          setNumPostsRewarded(numPostsRewarded);
+        }
+      }
+      if (mounted.current) {
+        setLoadingKarma(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userId]);
+  const instructionText = useMemo(() => {
+    if (authLevel < 2) {
+      return (
+        <span>
+          Your Karma Points = Total number of{' '}
+          <b style={{ color: Color.pink() }}>Twinkles</b> you rewarded + (
+          {recommendationsMultiplier} × total number of your{' '}
+          <b style={{ color: Color.brownOrange() }}>recommendations</b> that
+          were approved by teachers)
+        </span>
+      );
+    }
+    return (
+      <span>
+        Your Karma Points = Total number of <b>posts</b> you rewarded ×{' '}
+        {postsMultiplier}
+      </span>
+    );
+  }, [authLevel]);
+
+  const calculationText = useMemo(() => {
+    if (authLevel < 2) {
+      return (
+        <div style={{ fontSize: '1.5rem', marginTop: '3rem' }}>
+          <p>
+            Total number of <b style={{ color: Color.pink() }}>Twinkles</b> you
+            rewarded: {addCommasToNumber(numTwinklesRewarded)}
+          </p>
+          <p>
+            Total number of{' '}
+            <b style={{ color: Color.brownOrange() }}>recommendations</b>{' '}
+            approved by teachers:{' '}
+            {addCommasToNumber(numApprovedRecommendations)}
+          </p>
+          <p style={{ marginTop: '1rem', fontSize: '1.7rem' }}>
+            {numTwinklesRewarded} + ({recommendationsMultiplier} ×{' '}
+            {numApprovedRecommendations}) ={' '}
+            <b>{addCommasToNumber(karmaPoints)} Karma Points</b>
+          </p>
+        </div>
+      );
+    }
+    return (
+      <div style={{ fontSize: '1.5rem', marginTop: '3rem' }}>
+        <p>
+          Total number of posts you rewarded:{' '}
+          {addCommasToNumber(numPostsRewarded)}
+        </p>
+        <p style={{ marginTop: '1rem', fontSize: '1.7rem' }}>
+          {numPostsRewarded} × {postsMultiplier} ={' '}
+          <b>{addCommasToNumber(karmaPoints)} Karma Points</b>
+        </p>
+      </div>
+    );
+  }, [
+    authLevel,
+    karmaPoints,
+    numApprovedRecommendations,
+    numPostsRewarded,
+    numTwinklesRewarded
+  ]);
+
+  if (!userId) return null;
+
+  return (
+    <div
+      className={css`
+        background: #fff;
+        padding: 1rem;
+        border: 1px solid ${Color.borderGray()};
+        border-radius: ${borderRadius};
+        @media (max-width: ${mobileMaxWidth}) {
+          border-radius: 0;
+          border-top: 0;
+          border-left: 0;
+          border-right: 0;
+        }
+      `}
+    >
+      {loadingKarma ? (
+        <Loading style={{ height: '10rem' }} />
+      ) : (
+        <>
+          <p
+            className={css`
+              font-weight: bold;
+              font-size: 2.3rem;
+            `}
+          >
+            You have {addCommasToNumber(karmaPoints)} Karma Points
+          </p>
+          <div
+            className={css`
+              margin-top: 2rem;
+            `}
+          >
+            {userType && (
+              <p
+                className={css`
+                  font-size: 2rem;
+                  font-weight: bold;
+                `}
+              >
+                {userType}
+              </p>
+            )}
+            <p
+              className={css`
+                font-size: 1.7rem;
+              `}
+            >
+              {instructionText}
+            </p>
+            <div>{calculationText}</div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
