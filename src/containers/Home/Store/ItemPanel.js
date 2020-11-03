a4461940e84151e47ff4dde41b5a82957a2f8fa9
mikey1384 <mikey1384@gmail.com>
2020-11-03 16:29:02 +0900

    improve item panel css

--- src/containers/Home/Store/ItemPanel.js
+++ src/containers/Home/Store/ItemPanel.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from 'emotion';
+import { borderRadius, Color, mobileMaxWidth } from 'constants/css';
 
-export default function ItemPanel() {
+ItemPanel.propTypes = {
+  style: PropTypes.object
+};
+
+export default function ItemPanel({ style }) {
   return (
-    <div>
+    <div
+      className={css`
+        border-radius: ${borderRadius};
+        @media (max-width: ${mobileMaxWidth}) {
+          border-radius: 0;
+        }
+      `}
+      style={{
+        border: `1px solid ${Color.borderGray()}`,
+        padding: '1rem',
+        ...style
+      }}
+    >
       <div>This is an item</div>
     </div>
   );
--- src/containers/Home/Store/index.js
+++ src/containers/Home/Store/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import KarmaStatus from './KarmaStatus';
+import ItemPanel from './ItemPanel';
 
 export default function Store() {
   return (
     <div>
       <KarmaStatus />
+      <ItemPanel style={{ marginTop: '1rem' }} />
     </div>
   );
 }
