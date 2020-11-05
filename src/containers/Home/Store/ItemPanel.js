74764967d2d6350b2e0fff319b65a21e9cf2d946
mikey1384 <mikey1384@gmail.com>
2020-11-06 00:00:50 +0900

    minor fix

--- src/containers/Home/Store/ItemPanel.js
+++ src/containers/Home/Store/ItemPanel.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Icon from 'components/Icon';
+import Button from 'components/Button';
 import { css } from 'emotion';
 import { borderRadius, Color, mobileMaxWidth } from 'constants/css';
 
 ItemPanel.propTypes = {
   itemName: PropTypes.string.isRequired,
+  itemDescription: PropTypes.string.isRequired,
   karmaPoints: PropTypes.number,
   style: PropTypes.object
 };
 
-export default function ItemPanel({ itemName, style, karmaPoints }) {
+export default function ItemPanel({
+  itemName,
+  itemDescription,
+  style,
+  karmaPoints
+}) {
   return (
     <div
       className={css`
@@ -25,8 +33,34 @@ export default function ItemPanel({ itemName, style, karmaPoints }) {
         ...style
       }}
     >
+      <div style={{ fontWeight: 'bold', fontSize: '2rem' }}>{itemName}</div>
+      <p style={{ fontSize: '1.3rem' }}>{itemDescription}</p>
+      <div
+        style={{
+          marginTop: '2rem',
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: '1.7rem'
+        }}
+      >
+        <Icon size="3x" icon="lock" />
+        <div
+          style={{
+            marginTop: '1rem',
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'center'
+          }}
+        >
+          <Button skeuomorphic color="logoBlue">
+            Unlock
+          </Button>
+        </div>
+      </div>
       <div>You have {karmaPoints} karma points</div>
-      <div style={{ fontWeight: 'bold', fontSize: '1.7rem' }}>{itemName}</div>
     </div>
   );
 }
--- src/containers/Home/Store/index.js
+++ src/containers/Home/Store/index.js
@@ -14,6 +14,7 @@ export default function Store() {
       <ItemPanel
         karmaPoints={karmaPoints}
         itemName="Change your username"
+        itemDescription="Unlock this item to change your username anytime"
         style={{ marginTop: '1rem' }}
       />
     </div>
