5f906e9c6cd496259a484c7bac9de7befa2eb954
mikey1384 <mikey1384@gmail.com>
2020-11-10 21:15:13 +0900

    update

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,4 +1,4 @@
-export const clientVersion = '1.4.54';
+export const clientVersion = '1.4.55';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
