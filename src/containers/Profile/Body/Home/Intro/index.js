eef736ca82b6d53657d940c375897beaa4577527
mikey1384 <mikey1384@gmail.com>
2020-11-10 23:31:29 +0900

    refactor profile page

---
+++ src/containers/Profile/Body/Home/Intro/index.js
@@ -0,0 +1,396 @@
+import React, { useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import SectionPanel from 'components/SectionPanel';
+import StatusMsg from 'components/UserDetails/StatusMsg';
+import StatusInput from 'components/UserDetails/StatusInput';
+import RankBar from 'components/RankBar';
+import Button from 'components/Button';
+import Icon from 'components/Icon';
+import DropDownButton from 'components/Buttons/DropdownButton';
+import LoginToViewContent from 'components/LoginToViewContent';
+import BasicInfos from './BasicInfos';
+import ConfirmModal from 'components/Modals/ConfirmModal';
+import BioEditModal from 'components/Modals/BioEditModal';
+import Bio from 'components/Texts/Bio';
+import ErrorBoundary from 'components/ErrorBoundary';
+import request from 'axios';
+import URL from 'constants/URL';
+import { css } from 'emotion';
+import { useMyState } from 'helpers/hooks';
+import { Color, mobileMaxWidth } from 'constants/css';
+import { useAppContext, useContentContext, useInputContext } from 'contexts';
+import {
+  addEmoji,
+  renderText,
+  stringIsEmpty,
+  finalizeEmoji
+} from 'helpers/stringHelpers';
+
+Intro.propTypes = {
+  profile: PropTypes.object,
+  selectedTheme: PropTypes.string.isRequired
+};
+
+export default function Intro({ profile, selectedTheme }) {
+  const {
+    requestHelpers: { auth, uploadGreeting, uploadBio }
+  } = useAppContext();
+  const {
+    actions: {
+      onRemoveStatusMsg,
+      onUpdateStatusMsg,
+      onUpdateGreeting,
+      onUpdateBio
+    }
+  } = useContentContext();
+  const {
+    state: { editedStatusMsg, editedStatusColor },
+    actions: { onSetEditedStatusColor, onSetEditedStatusMsg }
+  } = useInputContext();
+  const { userId } = useMyState();
+  const [bioEditModalShown, setBioEditModalShown] = useState(false);
+  const [confirmModalShown, setConfirmModalShown] = useState(false);
+
+  const {
+    email,
+    id,
+    verifiedEmail,
+    greeting,
+    joinDate,
+    lastActive,
+    online,
+    profilePicId,
+    profileTheme,
+    profileFirstRow,
+    profileSecondRow,
+    profileThirdRow,
+    statusColor,
+    statusMsg,
+    username,
+    website,
+    youtubeName,
+    youtubeUrl
+  } = profile;
+
+  const StatusInputRef = useRef(null);
+  const bioExists = profileFirstRow || profileSecondRow || profileThirdRow;
+  const usernameColor = Color[selectedTheme]();
+  let defaultMessage = `<p>Welcome to <b style="color: ${usernameColor}">${username}</b>'s Profile Page</p>`;
+  const displayedStatusColor =
+    userId === profile.id ? editedStatusColor : statusColor;
+  const displayedStatusMsg =
+    userId === profile.id ? editedStatusMsg : statusMsg;
+
+  return (
+    <ErrorBoundary>
+      <SectionPanel
+        loaded
+        customColorTheme={selectedTheme}
+        title={greeting || 'Welcome!'}
+        canEdit={id === userId}
+        placeholder="Enter a message for your visitors"
+        onEditTitle={handleEditGreeting}
+      >
+        <div
+          style={{
+            display: 'flex',
+            minHeight: '10rem',
+            width: '100%',
+            marginTop: userId !== profile.id ? '-1.5rem' : 0
+          }}
+        >
+          <div
+            style={{
+              width: 'CALC(50% - 1rem)',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              marginRight: '1rem'
+            }}
+          >
+            <div
+              style={{
+                width: '90%',
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column'
+              }}
+            >
+              {userId === profile.id && (
+                <StatusInput
+                  innerRef={StatusInputRef}
+                  profile={profile}
+                  statusColor={editedStatusColor || statusColor}
+                  editedStatusMsg={editedStatusMsg}
+                  setColor={(color) => onSetEditedStatusColor(color)}
+                  onTextChange={(event) => {
+                    onSetEditedStatusMsg(
+                      addEmoji(renderText(event.target.value))
+                    );
+                    if (!event.target.value) {
+                      onSetEditedStatusColor('');
+                    }
+                  }}
+                  onCancel={() => {
+                    onSetEditedStatusMsg('');
+                    onSetEditedStatusColor('');
+                  }}
+                  onStatusSubmit={handleStatusMsgSubmit}
+                />
+              )}
+              {(!stringIsEmpty(statusMsg) || displayedStatusMsg) && (
+                <StatusMsg
+                  style={{
+                    fontSize: '1.6rem',
+                    width: '100%',
+                    marginTop: profile.twinkleXP > 0 || bioExists ? '1rem' : 0,
+                    marginBottom:
+                      profile.twinkleXP > 0 || bioExists ? '2rem' : 0
+                  }}
+                  statusColor={
+                    displayedStatusColor || statusColor || 'logoBlue'
+                  }
+                  statusMsg={displayedStatusMsg || statusMsg}
+                />
+              )}
+              {userId === profile.id &&
+                !editedStatusMsg &&
+                !stringIsEmpty(statusMsg) && (
+                  <div
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      marginTop: '-1rem',
+                      marginBottom:
+                        profile.twinkleXP > 0 || bioExists ? '1rem' : 0
+                    }}
+                  >
+                    <Button
+                      transparent
+                      onClick={() => {
+                        onSetEditedStatusMsg(statusMsg);
+                        StatusInputRef.current.focus();
+                      }}
+                    >
+                      <Icon icon="pencil-alt" />
+                      <span style={{ marginLeft: '0.7rem' }}>Edit</span>
+                    </Button>
+                    <Button
+                      transparent
+                      style={{ marginLeft: '0.5rem' }}
+                      onClick={() => setConfirmModalShown(true)}
+                    >
+                      <Icon icon="trash-alt" />
+                      <span style={{ marginLeft: '0.7rem' }}>Remove</span>
+                    </Button>
+                  </div>
+                )}
+              {userId !== profile.id && stringIsEmpty(statusMsg) && (
+                <div
+                  style={{
+                    marginTop: '-1rem',
+                    width: '100%',
+                    fontSize: '2rem',
+                    display: 'flex',
+                    textAlign: 'center',
+                    alignItems: 'center'
+                  }}
+                  dangerouslySetInnerHTML={{ __html: defaultMessage }}
+                />
+              )}
+            </div>
+          </div>
+          {userId ? (
+            <BasicInfos
+              profileTheme={profileTheme}
+              className={css`
+                font-size: 1.7rem;
+                margin-top: ${(!greeting || greeting.length) < 50
+                  ? userId === profile.id
+                    ? '-5rem'
+                    : '-2rem'
+                  : 0};
+                @media (max-width: ${mobileMaxWidth}) {
+                  font-size: 1.5rem;
+                  margin-top: ${(!greeting || greeting.length) < 20 &&
+                  userId === profile.id
+                    ? '-6rem'
+                    : '-3rem'};
+                }
+              `}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                width: 'CALC(50% - 1rem)',
+                marginLeft: '1rem',
+                marginBottom: '1rem'
+              }}
+              email={email}
+              verifiedEmail={verifiedEmail}
+              joinDate={joinDate}
+              online={online}
+              lastActive={lastActive}
+              profilePicId={profilePicId}
+              userId={id}
+              username={username}
+              selectedTheme={selectedTheme}
+              website={website}
+              youtubeName={youtubeName}
+              youtubeUrl={youtubeUrl}
+            />
+          ) : (
+            <LoginToViewContent />
+          )}
+        </div>
+        {profile.twinkleXP > 0 && (
+          <RankBar
+            profile={profile}
+            className={css`
+              margin-left: ${!!profile.rank && profile.rank < 4
+                ? '-11px'
+                : '-10px'};
+              margin-right: ${!!profile.rank && profile.rank < 4
+                ? '-11px'
+                : '-10px'};
+              @media (max-width: ${mobileMaxWidth}) {
+                margin-left: -1rem !important;
+                margin-right: -1rem !important;
+              }
+            `}
+            style={{
+              display: 'block',
+              borderRadius: 0,
+              borderRight: 0,
+              borderLeft: 0
+            }}
+          />
+        )}
+        {!profile.twinkleXP && bioExists && (
+          <hr
+            style={{
+              padding: '1px',
+              background: '#fff',
+              borderTop: `2px solid ${Color[selectedTheme || 'logoBlue'](0.6)}`,
+              borderBottom: `2px solid ${Color[selectedTheme || 'logoBlue'](
+                0.6
+              )}`
+            }}
+          />
+        )}
+        {bioExists && (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              position: 'relative'
+            }}
+          >
+            {userId === profile.id && (
+              <DropDownButton
+                direction="left"
+                opacity={0.7}
+                style={{ right: 0, top: '1rem', position: 'absolute' }}
+                skeuomorphic
+                color="darkerGray"
+                menuProps={[
+                  {
+                    label: 'Edit',
+                    onClick: () => setBioEditModalShown(true)
+                  },
+                  {
+                    label: 'Remove',
+                    onClick: () =>
+                      uploadBio({
+                        firstLine: '',
+                        secondLine: '',
+                        thirdLine: ''
+                      })
+                  }
+                ]}
+              />
+            )}
+            <Bio
+              style={{ fontSize: '1.6rem', marginBottom: '1rem' }}
+              firstRow={profileFirstRow}
+              secondRow={profileSecondRow}
+              thirdRow={profileThirdRow}
+            />
+          </div>
+        )}
+        {!bioExists && profile.id === userId && (
+          <div
+            style={{
+              width: '100%',
+              justifyContent: 'center',
+              display: 'flex',
+              marginTop: '1rem'
+            }}
+          >
+            <Button
+              style={{ fontSize: '2rem' }}
+              transparent
+              onClick={() => setBioEditModalShown(true)}
+            >
+              Add a Bio
+            </Button>
+          </div>
+        )}
+      </SectionPanel>
+      {bioEditModalShown && (
+        <BioEditModal
+          firstLine={profileFirstRow}
+          secondLine={profileSecondRow}
+          thirdLine={profileThirdRow}
+          onSubmit={handleUploadBio}
+          onHide={() => setBioEditModalShown(false)}
+        />
+      )}
+      {confirmModalShown && (
+        <ConfirmModal
+          onConfirm={handleRemoveStatus}
+          onHide={() => setConfirmModalShown(false)}
+          title={`Remove Status Message`}
+        />
+      )}
+    </ErrorBoundary>
+  );
+
+  async function handleUploadBio(params) {
+    const data = await uploadBio({
+      ...params,
+      profileId: profile.id
+    });
+    onUpdateBio(data);
+    setBioEditModalShown(false);
+  }
+
+  async function handleEditGreeting(greeting) {
+    await uploadGreeting({ greeting });
+    onUpdateGreeting({ greeting, userId });
+  }
+
+  async function handleRemoveStatus() {
+    await request.delete(`${URL}/user/statusMsg`, auth());
+    onRemoveStatusMsg(userId);
+    setConfirmModalShown(false);
+  }
+
+  async function handleStatusMsgSubmit() {
+    const statusMsg = finalizeEmoji(editedStatusMsg);
+    const statusColor = editedStatusColor || profile.statusColor;
+    const { data } = await request.post(
+      `${URL}/user/statusMsg`,
+      {
+        statusMsg,
+        statusColor
+      },
+      auth()
+    );
+    onSetEditedStatusColor('');
+    onSetEditedStatusMsg('');
+    onUpdateStatusMsg(data);
+  }
+}
--- src/containers/Profile/Body/Home/index.js
+++ src/containers/Profile/Body/Home/index.js
@@ -2,27 +2,9 @@ import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import SectionPanel from 'components/SectionPanel';
 import Comments from 'components/Comments';
-import StatusMsg from 'components/UserDetails/StatusMsg';
-import StatusInput from 'components/UserDetails/StatusInput';
-import RankBar from 'components/RankBar';
-import request from 'axios';
-import Button from 'components/Button';
-import Icon from 'components/Icon';
-import ConfirmModal from 'components/Modals/ConfirmModal';
-import BioEditModal from 'components/Modals/BioEditModal';
-import DropDownButton from 'components/Buttons/DropdownButton';
-import LoginToViewContent from 'components/LoginToViewContent';
+import Intro from './Intro';
 import { css } from 'emotion';
-import {
-  addEmoji,
-  finalizeEmoji,
-  renderText,
-  stringIsEmpty
-} from 'helpers/stringHelpers';
-import { Color, mobileMaxWidth } from 'constants/css';
-import URL from 'constants/URL';
-import Bio from 'components/Texts/Bio';
-import BasicInfos from './BasicInfos';
+import { mobileMaxWidth } from 'constants/css';
 import Achievements from './Achievements';
 import { useContentState, useMyState } from 'helpers/hooks';
 import { useAppContext, useContentContext, useInputContext } from 'contexts';
@@ -34,7 +16,7 @@ Home.propTypes = {
 
 export default function Home({ profile, selectedTheme }) {
   const {
-    requestHelpers: { auth, loadComments, uploadGreeting, uploadBio }
+    requestHelpers: { loadComments }
   } = useAppContext();
   const { userId } = useMyState();
   const {
@@ -47,45 +29,17 @@ export default function Home({ profile, selectedTheme }) {
       onLoadMoreComments,
       onLoadMoreReplies,
       onLoadRepliesOfReply,
-      onUpdateStatusMsg,
       onUploadComment,
-      onUploadReply,
-      onRemoveStatusMsg,
-      onUpdateGreeting,
-      onUpdateBio
+      onUploadReply
     }
   } = useContentContext();
   const {
-    state: { editedStatusMsg, editedStatusColor },
-    actions: { onSetEditedStatusColor, onSetEditedStatusMsg }
+    actions: { onSetEditedStatusColor }
   } = useInputContext();
-  const {
-    commentsLoaded,
-    email,
-    verifiedEmail,
-    greeting,
-    id,
-    joinDate,
-    lastActive,
-    online,
-    statusMsg,
-    profilePicId,
-    profileFirstRow,
-    profileSecondRow,
-    profileTheme,
-    profileThirdRow,
-    statusColor,
-    username,
-    website,
-    youtubeName,
-    youtubeUrl
-  } = profile;
+  const { commentsLoaded, id, username } = profile;
   const [loadingComments, setLoadingComments] = useState(false);
-  const [bioEditModalShown, setBioEditModalShown] = useState(false);
-  const [confirmModalShown, setConfirmModalShown] = useState(false);
   const mounted = useRef(true);
   const CommentInputAreaRef = useRef(null);
-  const StatusInputRef = useRef(null);
 
   useEffect(() => {
     return function cleanUp() {
@@ -131,14 +85,6 @@ export default function Home({ profile, selectedTheme }) {
     contentId: profile.id
   });
 
-  const bioExists = profileFirstRow || profileSecondRow || profileThirdRow;
-  const usernameColor = Color[selectedTheme]();
-  let defaultMessage = `<p>Welcome to <b style="color: ${usernameColor}">${username}</b>'s Profile Page</p>`;
-  const displayedStatusColor =
-    userId === profile.id ? editedStatusColor : statusColor;
-  const displayedStatusMsg =
-    userId === profile.id ? editedStatusMsg : statusMsg;
-
   return (
     <div
       className={css`
@@ -148,274 +94,12 @@ export default function Home({ profile, selectedTheme }) {
         }
       `}
     >
-      <SectionPanel
-        loaded
-        customColorTheme={selectedTheme}
-        title={greeting || 'Welcome!'}
-        canEdit={id === userId}
-        placeholder="Enter a message for your visitors"
-        onEditTitle={handleEditGreeting}
-      >
-        <div
-          style={{
-            display: 'flex',
-            minHeight: '10rem',
-            width: '100%',
-            marginTop: userId !== profile.id ? '-1.5rem' : 0
-          }}
-        >
-          <div
-            style={{
-              width: 'CALC(50% - 1rem)',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              marginRight: '1rem'
-            }}
-          >
-            <div
-              style={{
-                width: '90%',
-                height: '100%',
-                display: 'flex',
-                justifyContent: 'center',
-                flexDirection: 'column'
-              }}
-            >
-              {userId === profile.id && (
-                <StatusInput
-                  innerRef={StatusInputRef}
-                  profile={profile}
-                  statusColor={editedStatusColor || statusColor}
-                  editedStatusMsg={editedStatusMsg}
-                  setColor={(color) => onSetEditedStatusColor(color)}
-                  onTextChange={(event) => {
-                    onSetEditedStatusMsg(
-                      addEmoji(renderText(event.target.value))
-                    );
-                    if (!event.target.value) {
-                      onSetEditedStatusColor('');
-                    }
-                  }}
-                  onCancel={() => {
-                    onSetEditedStatusMsg('');
-                    onSetEditedStatusColor('');
-                  }}
-                  onStatusSubmit={handleStatusMsgSubmit}
-                />
-              )}
-              {(!stringIsEmpty(statusMsg) || displayedStatusMsg) && (
-                <StatusMsg
-                  style={{
-                    fontSize: '1.6rem',
-                    width: '100%',
-                    marginTop: profile.twinkleXP > 0 || bioExists ? '1rem' : 0,
-                    marginBottom:
-                      profile.twinkleXP > 0 || bioExists ? '2rem' : 0
-                  }}
-                  statusColor={
-                    displayedStatusColor || statusColor || 'logoBlue'
-                  }
-                  statusMsg={displayedStatusMsg || statusMsg}
-                />
-              )}
-              {userId === profile.id &&
-                !editedStatusMsg &&
-                !stringIsEmpty(statusMsg) && (
-                  <div
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      marginTop: '-1rem',
-                      marginBottom:
-                        profile.twinkleXP > 0 || bioExists ? '1rem' : 0
-                    }}
-                  >
-                    <Button
-                      transparent
-                      onClick={() => {
-                        onSetEditedStatusMsg(statusMsg);
-                        StatusInputRef.current.focus();
-                      }}
-                    >
-                      <Icon icon="pencil-alt" />
-                      <span style={{ marginLeft: '0.7rem' }}>Edit</span>
-                    </Button>
-                    <Button
-                      transparent
-                      style={{ marginLeft: '0.5rem' }}
-                      onClick={() => setConfirmModalShown(true)}
-                    >
-                      <Icon icon="trash-alt" />
-                      <span style={{ marginLeft: '0.7rem' }}>Remove</span>
-                    </Button>
-                  </div>
-                )}
-              {userId !== profile.id && stringIsEmpty(statusMsg) && (
-                <div
-                  style={{
-                    marginTop: '-1rem',
-                    width: '100%',
-                    fontSize: '2rem',
-                    display: 'flex',
-                    textAlign: 'center',
-                    alignItems: 'center'
-                  }}
-                  dangerouslySetInnerHTML={{ __html: defaultMessage }}
-                />
-              )}
-            </div>
-          </div>
-          {userId ? (
-            <BasicInfos
-              profileTheme={profileTheme}
-              className={css`
-                font-size: 1.7rem;
-                margin-top: ${(!greeting || greeting.length) < 50
-                  ? userId === profile.id
-                    ? '-5rem'
-                    : '-2rem'
-                  : 0};
-                @media (max-width: ${mobileMaxWidth}) {
-                  font-size: 1.5rem;
-                  margin-top: ${(!greeting || greeting.length) < 20 &&
-                  userId === profile.id
-                    ? '-6rem'
-                    : '-3rem'};
-                }
-              `}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                width: 'CALC(50% - 1rem)',
-                marginLeft: '1rem',
-                marginBottom: '1rem'
-              }}
-              email={email}
-              verifiedEmail={verifiedEmail}
-              joinDate={joinDate}
-              online={online}
-              lastActive={lastActive}
-              profilePicId={profilePicId}
-              userId={id}
-              username={username}
-              selectedTheme={selectedTheme}
-              website={website}
-              youtubeName={youtubeName}
-              youtubeUrl={youtubeUrl}
-            />
-          ) : (
-            <LoginToViewContent />
-          )}
-        </div>
-        {profile.twinkleXP > 0 && (
-          <RankBar
-            profile={profile}
-            className={css`
-              margin-left: ${!!profile.rank && profile.rank < 4
-                ? '-11px'
-                : '-10px'};
-              margin-right: ${!!profile.rank && profile.rank < 4
-                ? '-11px'
-                : '-10px'};
-              @media (max-width: ${mobileMaxWidth}) {
-                margin-left: -1rem !important;
-                margin-right: -1rem !important;
-              }
-            `}
-            style={{
-              display: 'block',
-              borderRadius: 0,
-              borderRight: 0,
-              borderLeft: 0
-            }}
-          />
-        )}
-        {!profile.twinkleXP && bioExists && (
-          <hr
-            style={{
-              padding: '1px',
-              background: '#fff',
-              borderTop: `2px solid ${Color[selectedTheme || 'logoBlue'](0.6)}`,
-              borderBottom: `2px solid ${Color[selectedTheme || 'logoBlue'](
-                0.6
-              )}`
-            }}
-          />
-        )}
-        {bioExists && (
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              position: 'relative'
-            }}
-          >
-            {userId === profile.id && (
-              <DropDownButton
-                direction="left"
-                opacity={0.7}
-                style={{ right: 0, top: '1rem', position: 'absolute' }}
-                skeuomorphic
-                color="darkerGray"
-                menuProps={[
-                  {
-                    label: 'Edit',
-                    onClick: () => setBioEditModalShown(true)
-                  },
-                  {
-                    label: 'Remove',
-                    onClick: () =>
-                      uploadBio({
-                        firstLine: '',
-                        secondLine: '',
-                        thirdLine: ''
-                      })
-                  }
-                ]}
-              />
-            )}
-            <Bio
-              style={{ fontSize: '1.6rem', marginBottom: '1rem' }}
-              firstRow={profileFirstRow}
-              secondRow={profileSecondRow}
-              thirdRow={profileThirdRow}
-            />
-          </div>
-        )}
-        {!bioExists && profile.id === userId && (
-          <div
-            style={{
-              width: '100%',
-              justifyContent: 'center',
-              display: 'flex',
-              marginTop: '1rem'
-            }}
-          >
-            <Button
-              style={{ fontSize: '2rem' }}
-              transparent
-              onClick={() => setBioEditModalShown(true)}
-            >
-              Add a Bio
-            </Button>
-          </div>
-        )}
-      </SectionPanel>
+      <Intro profile={profile} selectedTheme={selectedTheme} />
       <Achievements
         selectedTheme={selectedTheme}
         profile={profile}
         myId={userId}
       />
-      {confirmModalShown && (
-        <ConfirmModal
-          onConfirm={handleRemoveStatus}
-          onHide={() => setConfirmModalShown(false)}
-          title={`Remove Status Message`}
-        />
-      )}
       {(userId !== profile.id || comments.length > 0 || loadingComments) && (
         <SectionPanel
           customColorTheme={selectedTheme}
@@ -448,51 +132,6 @@ export default function Home({ profile, selectedTheme }) {
           />
         </SectionPanel>
       )}
-      {bioEditModalShown && (
-        <BioEditModal
-          firstLine={profileFirstRow}
-          secondLine={profileSecondRow}
-          thirdLine={profileThirdRow}
-          onSubmit={handleUploadBio}
-          onHide={() => setBioEditModalShown(false)}
-        />
-      )}
     </div>
   );
-
-  async function handleEditGreeting(greeting) {
-    await uploadGreeting({ greeting });
-    onUpdateGreeting({ greeting, userId });
-  }
-
-  async function handleUploadBio(params) {
-    const data = await uploadBio({
-      ...params,
-      profileId: profile.id
-    });
-    onUpdateBio(data);
-    setBioEditModalShown(false);
-  }
-
-  async function handleRemoveStatus() {
-    await request.delete(`${URL}/user/statusMsg`, auth());
-    onRemoveStatusMsg(userId);
-    setConfirmModalShown(false);
-  }
-
-  async function handleStatusMsgSubmit() {
-    const statusMsg = finalizeEmoji(editedStatusMsg);
-    const statusColor = editedStatusColor || profile.statusColor;
-    const { data } = await request.post(
-      `${URL}/user/statusMsg`,
-      {
-        statusMsg,
-        statusColor
-      },
-      auth()
-    );
-    onSetEditedStatusColor('');
-    onSetEditedStatusMsg('');
-    onUpdateStatusMsg(data);
-  }
 }
