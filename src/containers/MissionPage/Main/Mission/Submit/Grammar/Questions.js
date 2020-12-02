05b59c6360b53fbd7fb0941510b76aee42c21154
mikey1384 <mikey1384@gmail.com>
2020-12-02 17:44:31 +0900

    more refactoring

---
+++ src/containers/MissionPage/Main/Mission/Submit/Grammar/Questions.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import QuestionSlide from './QuestionSlide';
+import Carousel from 'components/Carousel';
+
+const choices = [
+  { label: 'graduate by', checked: false },
+  { label: 'graduate from', checked: false },
+  { label: 'graduating', checked: false },
+  { label: 'graduating from', checked: false }
+];
+
+export default function Questions() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  return (
+    <Carousel
+      allowDrag={false}
+      progressBar
+      slidesToShow={1}
+      slidesToScroll={1}
+      slideIndex={currentSlide}
+      afterSlide={setCurrentSlide}
+      onFinish={() => console.log('finished')}
+    >
+      <QuestionSlide choices={choices} />
+    </Carousel>
+  );
+}
--- src/containers/MissionPage/Main/Mission/Submit/Grammar/index.js
+++ src/containers/MissionPage/Main/Mission/Submit/Grammar/index.js
@@ -1,29 +1,10 @@
-import React, { useState } from 'react';
-import QuestionSlide from './QuestionSlide';
-import Carousel from 'components/Carousel';
-
-const choices = [
-  { label: 'graduate by', checked: false },
-  { label: 'graduate from', checked: false },
-  { label: 'graduating', checked: false },
-  { label: 'graduating from', checked: false }
-];
+import React from 'react';
+import Questions from './Questions';
 
 export default function Grammar() {
-  const [currentSlide, setCurrentSlide] = useState(0);
   return (
     <div>
-      <Carousel
-        allowDrag={false}
-        progressBar
-        slidesToShow={1}
-        slidesToScroll={1}
-        slideIndex={currentSlide}
-        afterSlide={setCurrentSlide}
-        onFinish={() => console.log('finished')}
-      >
-        <QuestionSlide choices={choices} />
-      </Carousel>
+      <Questions />
     </div>
   );
 }
