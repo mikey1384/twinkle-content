c6ca7c109b023b4894747f4a8973a5185e515d8d
mikey1384 <mikey1384@gmail.com>
2020-12-02 17:23:54 +0900

    more refactoring

--- src/containers/MissionPage/Main/Mission/Submit/Grammar/index.js
+++ src/containers/MissionPage/Main/Mission/Submit/Grammar/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import CheckListGroup from 'components/CheckListGroup';
-import { Color, borderRadius } from 'constants/css';
+import QuestionSlide from './QuestionSlide';
 
 const choices = [
   { label: 'graduate by', checked: false },
@@ -11,33 +10,8 @@ const choices = [
 
 export default function Grammar() {
   return (
-    <div
-      style={{
-        width: '100%',
-        border: `1px solid ${Color.borderGray()}`,
-        padding: '2rem 2rem 3rem 2rem',
-        borderRadius
-      }}
-    >
-      <div style={{ width: '100%' }}>
-        <div style={{ width: '100%', textAlign: 'center' }}>
-          <h3>What year did you _____ university?</h3>
-        </div>
-        <div
-          style={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            marginTop: '3rem'
-          }}
-        >
-          <CheckListGroup
-            style={{ marginTop: '1.5rem', width: '80%', fontSize: '1.6rem' }}
-            onSelect={(index) => console.log(index)}
-            listItems={choices}
-          />
-        </div>
-      </div>
+    <div>
+      <QuestionSlide choices={choices} />
     </div>
   );
 }
