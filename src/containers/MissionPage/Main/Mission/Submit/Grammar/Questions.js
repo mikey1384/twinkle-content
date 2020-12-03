8f3d1a8af7e35b6758972ae02f72bb4675238782
mikey1384 <mikey1384@gmail.com>
2020-12-03 23:16:24 +0900

    more updates

--- src/containers/MissionPage/Main/Mission/Submit/Grammar/Questions.js
+++ src/containers/MissionPage/Main/Mission/Submit/Grammar/Questions.js
@@ -3,33 +3,83 @@ import QuestionSlide from './QuestionSlide';
 import Carousel from 'components/Carousel';
 import { scrollElementToCenter } from 'helpers';
 
-const questionObj = {
-  0: {
-    objective:
-      'Choose the word or phrase that correctly completes the sentence',
-    question: 'What year did you _____ university?',
-    choices: [
-      { label: 'graduate by', checked: false },
-      { label: 'graduate from', checked: false },
-      { label: 'graduating', checked: false },
-      { label: 'graduating from', checked: false }
-    ]
-  },
-  1: {
-    objective: 'Another objective',
-    question: 'What year did you _____ university?',
-    choices: [
-      { label: 'graduate by', checked: false },
-      { label: 'graduate from', checked: false },
-      { label: 'graduating', checked: false },
-      { label: 'graduating from', checked: false }
-    ]
-  }
-};
-
-const questionIds = [0, 1];
-
+const questionIds = [0, 1, 2, 3, 4, 5];
 export default function Questions() {
+  const [questionObj] = useState({
+    0: {
+      id: 0,
+      objective:
+        'Choose the word or phrase that correctly completes the sentence',
+      question: 'What year did you _____ university?',
+      choices: [
+        { label: 'graduate by', checked: false },
+        { label: 'graduate from', checked: false },
+        { label: 'graduating', checked: false },
+        { label: 'graduating from', checked: false }
+      ]
+    },
+    1: {
+      id: 1,
+      objective:
+        'Choose the word or phrase that correctly completes the sentence',
+      question:
+        'It seems to be getting worse. You had better _____ a specialist.',
+      choices: [
+        { label: 'consult', checked: false },
+        { label: 'consult to', checked: false },
+        { label: 'consult for', checked: false },
+        { label: 'consult by', checked: false }
+      ]
+    },
+    2: {
+      id: 2,
+      objective:
+        'Choose the word or phrase that correctly completes the sentence',
+      question: 'Chicago is a large city, _____?',
+      choices: [
+        { label: `aren't it`, checked: false },
+        { label: `doesn't it`, checked: false },
+        { label: `won't it`, checked: false },
+        { label: `isn't it`, checked: false }
+      ]
+    },
+    3: {
+      id: 3,
+      objective:
+        'Choose the word or phrase that correctly completes the sentence',
+      question: `Don't leave your books near the open fire. They might easily _____.`,
+      choices: [
+        { label: 'catch to fire', checked: false },
+        { label: 'catch the fire', checked: false },
+        { label: 'catch on fire', checked: false },
+        { label: 'catch with fire', checked: false }
+      ]
+    },
+    4: {
+      id: 4,
+      objective:
+        'Choose the word or phrase that correctly completes the sentence',
+      question: 'Do you enjoy _____?',
+      choices: [
+        { label: 'to swim', checked: false },
+        { label: 'swimming', checked: false },
+        { label: 'swim', checked: false },
+        { label: 'to swimming', checked: false }
+      ]
+    },
+    5: {
+      id: 5,
+      objective:
+        'Choose the word or phrase that correctly completes the sentence',
+      question: 'I have trouble _____.',
+      choices: [
+        { label: 'to remember my password', checked: false },
+        { label: 'to remembering my password', checked: false },
+        { label: 'remember my password', checked: false },
+        { label: 'remembering my password', checked: false }
+      ]
+    }
+  });
   const QuestionsRef = useRef(null);
   const [currentSlide, setCurrentSlide] = useState(0);
   const [conditionPassStatus, setConditionPassStatus] = useState('');
