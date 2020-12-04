bb6e45cb724b2b3866a39b17728a8aaa65cf7017
mikey1384 <mikey1384@gmail.com>
2020-12-05 01:28:57 +0900

    more update

--- src/containers/MissionPage/Main/Mission/Submit/Grammar/Questions/StatusMessage.js
+++ src/containers/MissionPage/Main/Mission/Submit/Grammar/Questions/StatusMessage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Icon from 'components/Icon';
+import { Color } from 'constants/css';
 
 StatusMessage.propTypes = {
   status: PropTypes.string
@@ -7,8 +9,29 @@ StatusMessage.propTypes = {
 
 export default function StatusMessage({ status }) {
   return (
-    <div>
-      <div>this is a status message {status}</div>
+    <div
+      style={{
+        borderTop: `1px solid ${Color.borderGray()}`,
+        borderBottom: `1px solid ${Color.borderGray()}`,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '2rem',
+        marginLeft: '-1rem',
+        marginRight: '-1rem',
+        marginBottom: '-1rem',
+        fontSize: '1.5rem',
+        padding: '1.5rem'
+      }}
+    >
+      <Icon
+        size="2x"
+        style={{ color: status === 'fail' ? Color.rose() : Color.green() }}
+        icon={status === 'fail' ? 'times' : 'check'}
+      />
+      <p style={{ marginLeft: '2rem', fontSize: '1.7rem' }}>
+        this is a status message {status}
+      </p>
     </div>
   );
 }
