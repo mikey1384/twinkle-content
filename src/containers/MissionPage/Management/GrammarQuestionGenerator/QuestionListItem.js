bf5172337288e3945f544b61636a99d8a578d9b1
mikey1384 <mikey1384@gmail.com>
2020-12-13 17:18:49 +0900

    more fix

--- src/containers/MissionPage/Management/GrammarQuestionGenerator/QuestionListItem.js
+++ src/containers/MissionPage/Management/GrammarQuestionGenerator/QuestionListItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { Color, borderRadius, mobileMaxWidth } from 'constants/css';
 import { css } from 'emotion';
@@ -9,6 +9,15 @@ QuestionListItem.propTypes = {
 };
 
 export default function QuestionListItem({ question, style }) {
+  const correctAnswer = useMemo(() => {
+    return question.choices[question.answerIndex];
+  }, [question.answerIndex, question.choices]);
+  const wrongChoices = useMemo(() => {
+    return question.choices.filter(
+      (choice, index) => index !== question.answerIndex
+    );
+  }, [question.answerIndex, question.choices]);
+
   return (
     <div
       style={style}
@@ -16,7 +25,7 @@ export default function QuestionListItem({ question, style }) {
         width: 100%;
         background: #fff;
         border-radius: ${borderRadius};
-        padding: 1rem;
+        padding: 1.5rem 1rem 1.5rem 1rem;
         border: 1px solid ${Color.borderGray()};
         @media (max-width: ${mobileMaxWidth}) {
           border-left: 0;
@@ -25,11 +34,36 @@ export default function QuestionListItem({ question, style }) {
         }
       `}
     >
-      <h3>{question.question}</h3>
-      <div style={{ marginTop: '2rem' }}>
-        {question.choices.map((choice, index) => (
-          <div key={index}>{choice}</div>
-        ))}
+      <div style={{ width: '100%', textAlign: 'center' }}>
+        <p style={{ fontSize: '2.2rem', fontWeight: 'bold' }}>
+          {question.question}
+        </p>
+      </div>
+      <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+        <div
+          className={css`
+            > article {
+              text-align: center;
+              font-weight: bold;
+              font-size: 1.9rem;
+            }
+            p {
+              font-size: 1.7rem;
+            }
+          `}
+          style={{ marginTop: '3rem' }}
+        >
+          <article>Correct Choice</article>
+          <div style={{ marginTop: '0.5rem', textAlign: 'center' }}>
+            <p>{correctAnswer}</p>
+          </div>
+          <article style={{ marginTop: '3rem' }}>Wrong Choices</article>
+          <div style={{ marginTop: '0.5rem', textAlign: 'center' }}>
+            {wrongChoices.map((choice, index) => (
+              <p key={index}>{choice}</p>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
