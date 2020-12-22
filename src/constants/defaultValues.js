6151a1e2651c05b31bdea0cb98b7c227084ecdc3
mikey1384 <mikey1384@gmail.com>
2020-12-22 10:28:00 +0900

    grammar mission

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,4 +1,4 @@
-export const clientVersion = '1.4.61';
+export const clientVersion = '1.4.62';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
