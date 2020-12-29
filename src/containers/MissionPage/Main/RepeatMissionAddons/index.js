398b34e59b3443c6294e50ea738d02ecf9d8be5c
mikey1384 <mikey1384@gmail.com>
2020-12-29 16:50:47 +0900

    add add on tab filter bar

--- src/containers/MissionPage/Main/RepeatMissionAddons/index.js
+++ src/containers/MissionPage/Main/RepeatMissionAddons/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import GrammarReview from './GrammarReview';
+import FilterBar from 'components/FilterBar';
 
 RepeatMissionAddon.propTypes = {
   mission: PropTypes.object.isRequired,
@@ -10,11 +11,33 @@ RepeatMissionAddon.propTypes = {
 export default function RepeatMissionAddon({ mission, onSetMissionState }) {
   return (
     <div style={{ width: '100%' }}>
+      <FilterBar style={{ marginTop: '1.5rem' }} bordered>
+        <nav
+          onClick={() =>
+            onSetMissionState({
+              missionId: mission.id,
+              newState: { selectedAddonTab: 'grammarReview' }
+            })
+          }
+        >
+          Review
+        </nav>
+        <nav
+          onClick={() =>
+            onSetMissionState({
+              missionId: mission.id,
+              newState: { selectedAddonTab: 'rankings' }
+            })
+          }
+        >
+          Rankings
+        </nav>
+      </FilterBar>
       {(!mission.started || mission.failed) && (
         <GrammarReview
           mission={mission}
           onSetMissionState={onSetMissionState}
-          style={{ marginTop: '1.5rem' }}
+          style={{ marginTop: '1rem' }}
         />
       )}
     </div>
