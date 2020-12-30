17d2e0397c099eafaa8911ea8d4f524928148008
mikey1384 <mikey1384@gmail.com>
2020-12-30 21:02:22 +0900

    add grammar rankings component

---
+++ src/containers/MissionPage/Main/RepeatMissionAddons/GrammarRankings.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function GrammarRankings() {
+  return (
+    <div>
+      <div>this is grammar rankings component</div>
+    </div>
+  );
+}
--- src/containers/MissionPage/Main/RepeatMissionAddons/index.js
+++ src/containers/MissionPage/Main/RepeatMissionAddons/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import GrammarReview from './GrammarReview';
 import FilterBar from 'components/FilterBar';
 import ErrorBoundary from 'components/ErrorBoundary';
+import GrammarRankings from './GrammarRankings';
 import { useAppContext } from 'contexts';
 import { useMyState } from 'helpers/hooks';
 
@@ -120,6 +121,9 @@ export default function RepeatMissionAddon({ mission, onSetMissionState }) {
               onSetMissionState={onSetMissionState}
               style={{ marginTop: '1rem' }}
             />
+          )}
+          {(activeTab === 'rankings' || !missionAttemptExist) && (
+            <GrammarRankings />
           )}
         </ErrorBoundary>
       )}
