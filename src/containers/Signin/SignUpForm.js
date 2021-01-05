255c8ada6581feaea2f82d52feb4cabb5a705bab
mikey1384 <mikey1384@gmail.com>
2021-01-06 08:39:23 +0900

    minor fix

--- src/containers/Signin/SignUpForm.js
+++ src/containers/Signin/SignUpForm.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import ErrorBoundary from 'components/ErrorBoundary';
 import Button from 'components/Button';
 import Input from 'components/Texts/Input';
-import { css } from 'emotion';
 import Banner from 'components/Banner';
+import { css } from 'emotion';
 import { isValidUsername, stringIsEmpty } from 'helpers/stringHelpers';
 import { useAppContext, useContentContext } from 'contexts';
 
