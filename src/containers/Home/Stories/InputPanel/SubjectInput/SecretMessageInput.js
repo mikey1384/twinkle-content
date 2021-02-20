e5afb96b77378b595b0a4bb6a809b23e65a7c137
mikey1384 <mikey1384@gmail.com>
2021-02-20 21:37:14 +0900

    more updates

--- src/containers/Home/Stories/InputPanel/SubjectInput/SecretMessageInput.js
+++ src/containers/Home/Stories/InputPanel/SubjectInput/SecretMessageInput.js
@@ -1,11 +1,17 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Color } from 'constants/css';
-import { addEmoji, exceedsCharLimit } from 'helpers/stringHelpers';
+import {
+  addEmoji,
+  exceedsCharLimit,
+  addCommasToNumber
+} from 'helpers/stringHelpers';
 import { useMyState } from 'helpers/hooks';
+import { FILE_UPLOAD_XP_REQUIREMENT } from 'constants/defaultValues';
 import Icon from 'components/Icon';
 import Button from 'components/Button';
 import Textarea from 'components/Texts/Textarea';
+import FullTextReveal from 'components/Texts/FullTextReveal';
 
 SecretMessageInput.propTypes = {
   secretAnswer: PropTypes.string,
@@ -16,7 +22,9 @@ export default function SecretMessageInput({
   secretAnswer,
   onSetSecretAnswer
 }) {
-  const { profileTheme } = useMyState();
+  const [onHover, setOnHover] = useState(false);
+  const FileInputRef = useRef(null);
+  const { authLevel, profileTheme, twinkleXP, userId } = useMyState();
   const secretAnswerExceedsCharLimit = useMemo(
     () =>
       exceedsCharLimit({
@@ -25,6 +33,11 @@ export default function SecretMessageInput({
         text: secretAnswer
       }),
     [secretAnswer]
+  );
+  const disabled = useMemo(
+    () =>
+      !userId || (authLevel === 0 && twinkleXP < FILE_UPLOAD_XP_REQUIREMENT),
+    [authLevel, twinkleXP, userId]
   );
 
   return (
@@ -67,13 +80,100 @@ export default function SecretMessageInput({
         <div style={{ marginLeft: '1rem' }}>
           <Button
             skeuomorphic
+            disabled={disabled}
             color={profileTheme}
-            onClick={() => console.log('clicked')}
+            onClick={() => FileInputRef.current.click()}
+            onMouseEnter={() => setOnHover(true)}
+            onMouseLeave={() => setOnHover(false)}
           >
-            <Icon size="lg" icon="plus" />
+            <Icon size="lg" icon="upload" />
           </Button>
+          {userId && disabled && (
+            <FullTextReveal
+              style={{
+                fontSize: '1.3rem',
+                marginLeft: '1rem',
+                marginTop: '0.5rem'
+              }}
+              text={
+                'Requires ' +
+                addCommasToNumber(FILE_UPLOAD_XP_REQUIREMENT) +
+                ' XP'
+              }
+              show={onHover}
+            />
+          )}
         </div>
       </div>
+      <input
+        ref={FileInputRef}
+        style={{ display: 'none' }}
+        type="file"
+        onChange={handleUpload}
+      />
     </div>
   );
+
+  function handleUpload(event) {
+    console.log(event);
+    /*
+    const fileObj = event.target.files[0];
+    if (fileObj.size / mb > maxSize) {
+      return setAlertModalShown(true);
+    }
+    const { fileType } = getFileInfoFromFileName(fileObj.name);
+    if (fileType === 'image') {
+      const reader = new FileReader();
+      reader.onload = (upload) => {
+        const payload = upload.target.result;
+        if (fileObj.name.split('.')[1] === 'gif') {
+          onSetCommentAttachment({
+            attachment: {
+              file: fileObj,
+              contentType: 'file',
+              fileType,
+              imageUrl: payload
+            },
+            contentType,
+            contentId
+          });
+        } else {
+          window.loadImage(
+            payload,
+            function (img) {
+              const imageUrl = img.toDataURL('image/jpeg');
+              const dataUri = imageUrl.replace(/^data:image\/\w+;base64,/, '');
+              const buffer = Buffer.from(dataUri, 'base64');
+              const file = new File([buffer], fileObj.name);
+
+              onSetCommentAttachment({
+                attachment: {
+                  file,
+                  contentType: 'file',
+                  fileType,
+                  imageUrl
+                },
+                contentType,
+                contentId
+              });
+            },
+            { orientation: true, canvas: true }
+          );
+        }
+      };
+      reader.readAsDataURL(fileObj);
+    } else {
+      onSetCommentAttachment({
+        attachment: {
+          file: fileObj,
+          contentType: 'file',
+          fileType
+        },
+        contentType,
+        contentId
+      });
+    }
+    event.target.value = null;
+    */
+  }
 }
