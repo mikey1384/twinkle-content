0c44ecca813000e2f8b7f280d458ccc065ac1b0a
mikey1384 <mikey1384@gmail.com>
2021-02-27 13:15:35 +0900

    more updates

--- src/components/Comments/PinnedComment.js
+++ src/components/Comments/PinnedComment.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useContentState } from 'helpers/hooks';
 import PropTypes from 'prop-types';
 import Comment from './Comment';
+import { useAppContext, useContentContext } from 'contexts';
 
 PinnedComment.propTypes = {
   commentId: PropTypes.number.isRequired,
@@ -18,6 +19,12 @@ export default function PinnedComment({
   subject,
   userId
 }) {
+  const {
+    requestHelpers: { loadContent }
+  } = useAppContext();
+  const {
+    actions: { onInitContent }
+  } = useContentContext();
   const comment = useContentState({
     contentType: 'comment',
     contentId: commentId
@@ -25,23 +32,28 @@ export default function PinnedComment({
   const [loaded, setLoaded] = useState(false);
   useEffect(() => {
     if (!comment.loaded) {
-      return console.log('gotta load');
+      return init();
     }
     setLoaded(true);
-  }, [comment]);
 
-  return (
-    <div>
-      {loaded && (
-        <Comment
-          parent={parent}
-          rootContent={rootContent}
-          subject={subject}
-          comment={comment}
-          userId={userId}
-          isPinned
-        />
-      )}
-    </div>
-  );
+    async function init() {
+      const data = await loadContent({
+        contentId: commentId,
+        contentType: 'comment'
+      });
+      onInitContent({ contentId: commentId, contentType: 'comment', ...data });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [comment, commentId]);
+
+  return loaded ? (
+    <Comment
+      parent={parent}
+      rootContent={rootContent}
+      subject={subject}
+      comment={comment}
+      userId={userId}
+      isPinned
+    />
+  ) : null;
 }
