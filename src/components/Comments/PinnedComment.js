14fcd6ac3ab3d0d2c827b1ce15cfd432bfe1ad6d
mikey1384 <mikey1384@gmail.com>
2021-02-27 09:18:24 +0900

    more updates

--- src/components/Comments/PinnedComment.js
+++ src/components/Comments/PinnedComment.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useContentState } from 'helpers/hooks';
 import PropTypes from 'prop-types';
 
 PinnedComment.propTypes = {
@@ -6,9 +7,23 @@ PinnedComment.propTypes = {
 };
 
 export default function PinnedComment({ commentId }) {
+  const comment = useContentState({
+    contentType: 'comment',
+    contentId: commentId
+  });
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    if (!comment.loaded) {
+      return console.log('gotta load');
+    }
+    setLoaded(true);
+  }, [comment]);
+
   return (
     <div>
-      <div>this is where the pinned comment is gonna go {commentId}</div>
+      {loaded && (
+        <div>this is where the pinned comment is gonna go {commentId}</div>
+      )}
     </div>
   );
 }
