eeca48d1981abd097f4d803c3ec8a68350e120d4
mikey1384 <mikey1384@gmail.com>
2021-03-02 14:36:23 +0900

    more updates

--- src/containers/MissionPage/Main/Mission/Main/TwinkleStore.js
+++ src/containers/MissionPage/Main/Mission/Main/TwinkleStore.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import UsernameChangeItem from './username_change_item.png';
 
 TwinkleStore.propTypes = {
   mission: PropTypes.object.isRequired
@@ -8,6 +9,7 @@ TwinkleStore.propTypes = {
 export default function TwinkleStore({ mission }) {
   return (
     <div>
+      <img style={{ width: '100%' }} src={UsernameChangeItem} />
       <div>this is {mission.title}</div>
     </div>
   );
