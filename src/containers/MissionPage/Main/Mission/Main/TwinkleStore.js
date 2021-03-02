736c9681418581fb110cc97157a760674596b900
mikey1384 <mikey1384@gmail.com>
2021-03-02 19:30:05 +0900

    more updates

--- src/containers/MissionPage/Main/Mission/Main/TwinkleStore.js
+++ src/containers/MissionPage/Main/Mission/Main/TwinkleStore.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import UsernameChangeItem from './username_change_item.png';
+import { mobileMaxWidth } from 'constants/css';
+import { css } from '@emotion/css';
 
 TwinkleStore.propTypes = {
   mission: PropTypes.object.isRequired
@@ -8,9 +10,16 @@ TwinkleStore.propTypes = {
 
 export default function TwinkleStore({ mission }) {
   return (
-    <div style={{ fontSize: '1.7rem' }}>
-      <p style={{ fontWeight: 'bold', fontSize: '2rem' }}>Instructions:</p>
-      <p style={{ marginTop: '1.5rem' }}>
+    <div
+      style={{
+        fontSize: '1.7rem',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <p style={{ fontWeight: 'bold', fontSize: '2.3rem' }}>Instructions</p>
+      <p style={{ marginTop: '2.5rem' }}>
         <span>In </span>
         <a href="/store" target="_blank">
           Twinkle Store
@@ -18,7 +27,14 @@ export default function TwinkleStore({ mission }) {
         <span>{`, you will see a section labeled "change your username"`}</span>
       </p>
       <img
-        style={{ width: '100%', marginTop: '1rem' }}
+        className={css`
+          width: 100%;
+          max-width: 50rem;
+          margin-top: 1.5rem;
+          @media (max-width: ${mobileMaxWidth}) {
+            max-width: 100%;
+          }
+        `}
         src={UsernameChangeItem}
       />
       <div>this is {mission.title}</div>
