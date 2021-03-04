c7ea9eebef51b7de1ec4777355c7336b9ee3ab6f
mikey1384 <mikey1384@gmail.com>
2021-03-04 21:16:48 +0900

    more updates

--- src/containers/MissionPage/Main/Mission/Main/TwinkleStore/FinalStep.js
+++ src/containers/MissionPage/Main/Mission/Main/TwinkleStore/FinalStep.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'components/Button';
+import { css } from '@emotion/css';
 
 FinalStep.propTypes = {
   style: PropTypes.object
@@ -14,12 +15,22 @@ export default function FinalStep({ style }) {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+        fontSize: '1.7rem',
         ...style
       }}
+      className={css`
+        > p {
+          line-height: 2;
+        }
+      `}
     >
       <p>Great! You have unlocked change username item from Twinkle Store!</p>
       <p>Press this button to claim your reward</p>
-      <Button skeuomorphic color="green">
+      <Button
+        style={{ marginTop: '2rem', fontSize: '1.7rem' }}
+        skeuomorphic
+        color="green"
+      >
         Press it
       </Button>
     </div>
