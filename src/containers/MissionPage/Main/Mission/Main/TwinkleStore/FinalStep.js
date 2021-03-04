3b0fe522288832cf05261e4e73d282e17f216870
mikey1384 <mikey1384@gmail.com>
2021-03-04 20:33:47 +0900

    more updates

--- src/containers/MissionPage/Main/Mission/Main/TwinkleStore/FinalStep.js
+++ src/containers/MissionPage/Main/Mission/Main/TwinkleStore/FinalStep.js
@@ -3,14 +3,19 @@ import Button from 'components/Button';
 
 export default function FinalStep() {
   return (
-    <div>
-      Great! You have unlocked change username item from Twinkle Store!
-      <div>
-        Press this button to claim your reward
-        <Button skeuomorphic color="green">
-          Press it
-        </Button>
-      </div>
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <p>Great! You have unlocked change username item from Twinkle Store!</p>
+      <p>Press this button to claim your reward</p>
+      <Button skeuomorphic color="green">
+        Press it
+      </Button>
     </div>
   );
 }
