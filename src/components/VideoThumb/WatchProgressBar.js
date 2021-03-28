66a4ea2c2de4fc072e17d25f8c56e1f438ecaa00
mikey1384 <mikey1384@gmail.com>
2021-03-28 23:15:45 +0900

    more updates

--- src/components/VideoThumb/WatchProgressBar.js
+++ src/components/VideoThumb/WatchProgressBar.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Color } from 'constants/css';
+import { Color, mobileMaxWidth } from 'constants/css';
+import { css } from '@emotion/css';
 
 export default function WatchProgressBar() {
   return (
     <div style={{ position: 'absolute', width: '100%', bottom: 0 }}>
-      <div style={{ background: Color.red(), height: '5px', width: '100%' }} />
+      <div
+        className={css`
+          background: ${Color.red()};
+          height: 5px;
+          width: 100%;
+          @media (max-width: ${mobileMaxWidth}) {
+            height: 3px;
+          }
+        `}
+      />
     </div>
   );
 }
