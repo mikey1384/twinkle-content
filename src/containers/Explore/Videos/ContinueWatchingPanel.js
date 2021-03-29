3fa0f7f7f6c32f4518f6788c856e17be512a435d
mikey1384 <mikey1384@gmail.com>
2021-03-29 19:40:17 +0900

    more updates

--- src/containers/Explore/Videos/ContinueWatchingPanel.js
+++ src/containers/Explore/Videos/ContinueWatchingPanel.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ErrorBoundary from 'components/ErrorBoundary';
+import SectionPanel from 'components/SectionPanel';
 
 export default function ContinueWatchingPanel() {
   return (
     <ErrorBoundary>
-      <div>This is continue watching panel</div>
+      <SectionPanel
+        title="Continue Watching"
+        isEmpty={false}
+        loaded={true}
+        onLoadMore={() => console.log('plz load more')}
+        loadMoreButtonShown={true}
+      ></SectionPanel>
     </ErrorBoundary>
   );
 }
