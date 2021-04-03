f661060f232735d92f78c37079ec04e0991a055a
mikey1384 <mikey1384@gmail.com>
2021-04-03 21:19:54 +0900

    performance update for chess messages

--- src/containers/Chat/Chess/Board.js
+++ src/containers/Chat/Chess/Board.js
@@ -1,4 +1,4 @@
-import React, { memo, Fragment, useMemo } from 'react';
+import React, { memo, useCallback, Fragment, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Square from './Square';
 import getPiece from './helpers/piece';
@@ -31,6 +31,246 @@ function Board({
   opponentName,
   spoilerOff
 }) {
+  const renderCastlingButtons = useCallback(() => {
+    const top = 'CALC(100% - 6rem)';
+    const mobileTop = 'CALC(50vw + 0.5rem)';
+    const mobileCastlingBackgroundColor = Color.pink(0.7);
+    return myColor === 'white' ? (
+      <>
+        {interactable &&
+          !squares[57].isPiece &&
+          !squares[58].isPiece &&
+          !squares[59].isPiece &&
+          squares[56].type === 'rook' &&
+          !squares[56].moved &&
+          squares[60].type === 'king' &&
+          squares[60].state !== 'check' &&
+          squares[60].state !== 'checkmate' &&
+          !squares[60].moved && (
+            <div
+              className={css`
+                cursor: pointer;
+                position: absolute;
+                background: RGB(255, 255, 255, 0.7);
+                top: ${top};
+                left: 89px;
+                display: flex;
+                align-items: center;
+                padding: 0 0.5rem 0 0.5rem;
+                @media (max-width: ${mobileMaxWidth}) {
+                  background: ${mobileCastlingBackgroundColor};
+                  font-size: 1rem;
+                  left: 0;
+                  top: ${mobileTop};
+                }
+              `}
+              onClick={() => onCastling('left')}
+            >
+              ←{' '}
+              <img
+                className={css`
+                  width: 2.5rem;
+                  height: 2.5rem;
+                  @media (max-width: ${mobileMaxWidth}) {
+                    width: 2rem;
+                    height: 2rem;
+                  }
+                `}
+                src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"
+                alt=""
+              />
+              <img
+                className={css`
+                  width: 2.5rem;
+                  height: 2.5rem;
+                  @media (max-width: ${mobileMaxWidth}) {
+                    width: 2rem;
+                    height: 2rem;
+                  }
+                `}
+                src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg"
+                alt=""
+              />{' '}
+              →
+            </div>
+          )}
+        {interactable &&
+          !squares[61].isPiece &&
+          !squares[62].isPiece &&
+          squares[63].type === 'rook' &&
+          !squares[63].moved &&
+          squares[60].type === 'king' &&
+          squares[60].state !== 'check' &&
+          squares[60].state !== 'checkmate' &&
+          !squares[60].moved && (
+            <div
+              className={css`
+                cursor: pointer;
+                position: absolute;
+                background: RGB(255, 255, 255, 0.7);
+                top: ${top};
+                left: 246px;
+                display: flex;
+                align-items: center;
+                padding: 0 0.5rem 0 0.5rem;
+                @media (max-width: ${mobileMaxWidth}) {
+                  background: ${mobileCastlingBackgroundColor};
+                  font-size: 1rem;
+                  left: CALC(100% - 7rem);
+                  top: ${mobileTop};
+                }
+              `}
+              onClick={() => onCastling('right')}
+            >
+              ←{' '}
+              <img
+                className={css`
+                  width: 2.5rem;
+                  height: 2.5rem;
+                  @media (max-width: ${mobileMaxWidth}) {
+                    width: 2rem;
+                    height: 2rem;
+                  }
+                `}
+                src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg"
+                alt=""
+              />
+              <img
+                className={css`
+                  width: 2.5rem;
+                  height: 2.5rem;
+                  @media (max-width: ${mobileMaxWidth}) {
+                    width: 2rem;
+                    height: 2rem;
+                  }
+                `}
+                src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"
+                alt=""
+              />{' '}
+              →
+            </div>
+          )}
+      </>
+    ) : (
+      <>
+        {interactable &&
+          !squares[57].isPiece &&
+          !squares[58].isPiece &&
+          squares[56].type === 'rook' &&
+          !squares[56].moved &&
+          squares[59].type === 'king' &&
+          squares[59].state !== 'check' &&
+          squares[59].state !== 'checkmate' &&
+          !squares[59].moved && (
+            <div
+              className={css`
+                cursor: pointer;
+                position: absolute;
+                background: RGB(255, 255, 255, 0.7);
+                top: ${top};
+                left: 66px;
+                display: flex;
+                align-items: center;
+                padding: 0 0.5rem 0 0.5rem;
+                @media (max-width: ${mobileMaxWidth}) {
+                  background: ${mobileCastlingBackgroundColor};
+                  font-size: 1rem;
+                  left: 0;
+                  top: ${mobileTop};
+                }
+              `}
+              onClick={() => onCastling('left')}
+            >
+              ←{' '}
+              <img
+                className={css`
+                  width: 2.5rem;
+                  height: 2.5rem;
+                  @media (max-width: ${mobileMaxWidth}) {
+                    width: 2rem;
+                    height: 2rem;
+                  }
+                `}
+                src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"
+                alt=""
+              />
+              <img
+                className={css`
+                  width: 2.5rem;
+                  height: 2.5rem;
+                  @media (max-width: ${mobileMaxWidth}) {
+                    width: 2rem;
+                    height: 2rem;
+                  }
+                `}
+                src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg"
+                alt=""
+              />{' '}
+              →
+            </div>
+          )}
+        {interactable &&
+          !squares[60].isPiece &&
+          !squares[61].isPiece &&
+          !squares[62].isPiece &&
+          squares[63].type === 'rook' &&
+          !squares[63].moved &&
+          squares[59].type === 'king' &&
+          squares[59].state !== 'check' &&
+          squares[59].state !== 'checkmate' &&
+          !squares[59].moved && (
+            <div
+              className={css`
+                cursor: pointer;
+                position: absolute;
+                background: RGB(255, 255, 255, 0.7);
+                top: ${top};
+                left: 223px;
+                display: flex;
+                align-items: center;
+                padding: 0 0.5rem 0 0.5rem;
+                @media (max-width: ${mobileMaxWidth}) {
+                  background: ${mobileCastlingBackgroundColor};
+                  font-size: 1rem;
+                  left: CALC(100% - 7rem);
+                  top: ${mobileTop};
+                }
+              `}
+              onClick={() => onCastling('right')}
+            >
+              ←{' '}
+              <img
+                className={css`
+                  width: 2.5rem;
+                  height: 2.5rem;
+                  @media (max-width: ${mobileMaxWidth}) {
+                    width: 2rem;
+                    height: 2rem;
+                  }
+                `}
+                src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg"
+                alt=""
+              />
+              <img
+                className={css`
+                  width: 2.5rem;
+                  height: 2.5rem;
+                  @media (max-width: ${mobileMaxWidth}) {
+                    width: 2rem;
+                    height: 2rem;
+                  }
+                `}
+                src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"
+                alt=""
+              />{' '}
+              →
+            </div>
+          )}
+      </>
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [interactable, myColor, squares]);
+
   const board = useMemo(() => {
     const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     function isEven(num) {
@@ -210,244 +450,6 @@ function Board({
       );
     } else return null;
 
-    function renderCastlingButtons() {
-      const top = 'CALC(100% - 6rem)';
-      const mobileTop = 'CALC(50vw + 0.5rem)';
-      const mobileCastlingBackgroundColor = Color.pink(0.7);
-      return myColor === 'white' ? (
-        <>
-          {interactable &&
-            !squares[57].isPiece &&
-            !squares[58].isPiece &&
-            !squares[59].isPiece &&
-            squares[56].type === 'rook' &&
-            !squares[56].moved &&
-            squares[60].type === 'king' &&
-            squares[60].state !== 'check' &&
-            squares[60].state !== 'checkmate' &&
-            !squares[60].moved && (
-              <div
-                className={css`
-                  cursor: pointer;
-                  position: absolute;
-                  background: RGB(255, 255, 255, 0.7);
-                  top: ${top};
-                  left: 89px;
-                  display: flex;
-                  align-items: center;
-                  padding: 0 0.5rem 0 0.5rem;
-                  @media (max-width: ${mobileMaxWidth}) {
-                    background: ${mobileCastlingBackgroundColor};
-                    font-size: 1rem;
-                    left: 0;
-                    top: ${mobileTop};
-                  }
-                `}
-                onClick={() => onCastling('left')}
-              >
-                ←{' '}
-                <img
-                  className={css`
-                    width: 2.5rem;
-                    height: 2.5rem;
-                    @media (max-width: ${mobileMaxWidth}) {
-                      width: 2rem;
-                      height: 2rem;
-                    }
-                  `}
-                  src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"
-                  alt=""
-                />
-                <img
-                  className={css`
-                    width: 2.5rem;
-                    height: 2.5rem;
-                    @media (max-width: ${mobileMaxWidth}) {
-                      width: 2rem;
-                      height: 2rem;
-                    }
-                  `}
-                  src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg"
-                  alt=""
-                />{' '}
-                →
-              </div>
-            )}
-          {interactable &&
-            !squares[61].isPiece &&
-            !squares[62].isPiece &&
-            squares[63].type === 'rook' &&
-            !squares[63].moved &&
-            squares[60].type === 'king' &&
-            squares[60].state !== 'check' &&
-            squares[60].state !== 'checkmate' &&
-            !squares[60].moved && (
-              <div
-                className={css`
-                  cursor: pointer;
-                  position: absolute;
-                  background: RGB(255, 255, 255, 0.7);
-                  top: ${top};
-                  left: 246px;
-                  display: flex;
-                  align-items: center;
-                  padding: 0 0.5rem 0 0.5rem;
-                  @media (max-width: ${mobileMaxWidth}) {
-                    background: ${mobileCastlingBackgroundColor};
-                    font-size: 1rem;
-                    left: CALC(100% - 7rem);
-                    top: ${mobileTop};
-                  }
-                `}
-                onClick={() => onCastling('right')}
-              >
-                ←{' '}
-                <img
-                  className={css`
-                    width: 2.5rem;
-                    height: 2.5rem;
-                    @media (max-width: ${mobileMaxWidth}) {
-                      width: 2rem;
-                      height: 2rem;
-                    }
-                  `}
-                  src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg"
-                  alt=""
-                />
-                <img
-                  className={css`
-                    width: 2.5rem;
-                    height: 2.5rem;
-                    @media (max-width: ${mobileMaxWidth}) {
-                      width: 2rem;
-                      height: 2rem;
-                    }
-                  `}
-                  src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"
-                  alt=""
-                />{' '}
-                →
-              </div>
-            )}
-        </>
-      ) : (
-        <>
-          {interactable &&
-            !squares[57].isPiece &&
-            !squares[58].isPiece &&
-            squares[56].type === 'rook' &&
-            !squares[56].moved &&
-            squares[59].type === 'king' &&
-            squares[59].state !== 'check' &&
-            squares[59].state !== 'checkmate' &&
-            !squares[59].moved && (
-              <div
-                className={css`
-                  cursor: pointer;
-                  position: absolute;
-                  background: RGB(255, 255, 255, 0.7);
-                  top: ${top};
-                  left: 66px;
-                  display: flex;
-                  align-items: center;
-                  padding: 0 0.5rem 0 0.5rem;
-                  @media (max-width: ${mobileMaxWidth}) {
-                    background: ${mobileCastlingBackgroundColor};
-                    font-size: 1rem;
-                    left: 0;
-                    top: ${mobileTop};
-                  }
-                `}
-                onClick={() => onCastling('left')}
-              >
-                ←{' '}
-                <img
-                  className={css`
-                    width: 2.5rem;
-                    height: 2.5rem;
-                    @media (max-width: ${mobileMaxWidth}) {
-                      width: 2rem;
-                      height: 2rem;
-                    }
-                  `}
-                  src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"
-                  alt=""
-                />
-                <img
-                  className={css`
-                    width: 2.5rem;
-                    height: 2.5rem;
-                    @media (max-width: ${mobileMaxWidth}) {
-                      width: 2rem;
-                      height: 2rem;
-                    }
-                  `}
-                  src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg"
-                  alt=""
-                />{' '}
-                →
-              </div>
-            )}
-          {interactable &&
-            !squares[60].isPiece &&
-            !squares[61].isPiece &&
-            !squares[62].isPiece &&
-            squares[63].type === 'rook' &&
-            !squares[63].moved &&
-            squares[59].type === 'king' &&
-            squares[59].state !== 'check' &&
-            squares[59].state !== 'checkmate' &&
-            !squares[59].moved && (
-              <div
-                className={css`
-                  cursor: pointer;
-                  position: absolute;
-                  background: RGB(255, 255, 255, 0.7);
-                  top: ${top};
-                  left: 223px;
-                  display: flex;
-                  align-items: center;
-                  padding: 0 0.5rem 0 0.5rem;
-                  @media (max-width: ${mobileMaxWidth}) {
-                    background: ${mobileCastlingBackgroundColor};
-                    font-size: 1rem;
-                    left: CALC(100% - 7rem);
-                    top: ${mobileTop};
-                  }
-                `}
-                onClick={() => onCastling('right')}
-              >
-                ←{' '}
-                <img
-                  className={css`
-                    width: 2.5rem;
-                    height: 2.5rem;
-                    @media (max-width: ${mobileMaxWidth}) {
-                      width: 2rem;
-                      height: 2rem;
-                    }
-                  `}
-                  src="https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg"
-                  alt=""
-                />
-                <img
-                  className={css`
-                    width: 2.5rem;
-                    height: 2.5rem;
-                    @media (max-width: ${mobileMaxWidth}) {
-                      width: 2rem;
-                      height: 2rem;
-                    }
-                  `}
-                  src="https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg"
-                  alt=""
-                />{' '}
-                →
-              </div>
-            )}
-        </>
-      );
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [interactable, myColor, opponentName, spoilerOff, squares]);
 
