dede8693d4c81d31eb044513643283717c8b4587
mikey1384 <mikey1384@gmail.com>
2021-04-22 17:22:01 +0900

    more updates

--- src/containers/MissionPage/Main/index.js
+++ src/containers/MissionPage/Main/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Loading from 'components/Loading';
 import ErrorBoundary from 'components/ErrorBoundary';
 import SingleMissionPage from './SingleMissionPage';
+import { Switch, Route } from 'react-router-dom';
+import MultiMissionPage from './MultiMissionPage';
 
 Main.propTypes = {
   mission: PropTypes.object.isRequired,
@@ -14,10 +16,22 @@ export default function Main({ mission, onSetMissionState, style }) {
   return (
     <ErrorBoundary style={{ width: '100%', ...style }}>
       {mission ? (
-        <SingleMissionPage
-          mission={mission}
-          onSetMissionState={onSetMissionState}
-        />
+        <Switch>
+          <Route
+            path={`/missions/${mission.missionType}/:subMissionId`}
+            render={() => <MultiMissionPage />}
+          />
+          <Route
+            exact
+            path={`/missions/${mission.missionType}`}
+            render={() => (
+              <SingleMissionPage
+                mission={mission}
+                onSetMissionState={onSetMissionState}
+              />
+            )}
+          />
+        </Switch>
       ) : (
         <Loading text="Loading Mission..." />
       )}
