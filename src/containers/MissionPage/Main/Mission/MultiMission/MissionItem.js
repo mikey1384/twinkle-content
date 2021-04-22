4ee71811304c0924993ad482f3e69bbac09902b6
mikey1384 <mikey1384@gmail.com>
2021-04-22 16:36:35 +0900

    more updates

--- src/containers/MissionPage/Main/Mission/MultiMission/MissionItem.js
+++ src/containers/MissionPage/Main/Mission/MultiMission/MissionItem.js
@@ -139,7 +139,7 @@ export default function MissionItem({
 
   function handleLinkClick() {
     if (userId) {
-      history.push(`/missions/${mission.missionType}/1`);
+      history.push(`/missions/${mission.missionType}/${mission.id}`);
     } else {
       onOpenSigninModal();
     }
