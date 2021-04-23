f574e5b6a9196e4b54036279b698afb3fd6c86d4
mikey1384 <mikey1384@gmail.com>
2021-04-23 15:56:11 +0900

    more updates

--- src/containers/MissionPage/Main/MultiMissionPage/index.js
+++ src/containers/MissionPage/Main/MultiMissionPage/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useMyState } from 'helpers/hooks';
 
 export default function MultiMissionPage() {
-  const { status } = useMyState();
-  console.log(status);
   return (
     <div>
       <div>This is multi mission page</div>
