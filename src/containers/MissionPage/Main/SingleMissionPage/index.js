a2174817456815a120e34e1f93197395ba9ae15d
mikey1384 <mikey1384@gmail.com>
2021-04-27 18:25:27 +0900

    more refactoring

--- src/containers/MissionPage/Main/SingleMissionPage/index.js
+++ src/containers/MissionPage/Main/SingleMissionPage/index.js
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Mission from './Mission';
 import Tutorial from '../Tutorial';
-import RepeatMissionAddons from './RepeatMissionAddons';
+import RepeatMissionAddons from '../MissionModules/RepeatMissionAddons';
 import { mobileMaxWidth } from 'constants/css';
 import { css } from '@emotion/css';
 
