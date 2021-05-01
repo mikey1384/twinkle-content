946a28f8edcdd8c770e5b456eea87109e8283803
mikey1384 <mikey1384@gmail.com>
2021-05-01 22:06:10 +0900

    more updates

--- src/containers/MissionPage/Main/MissionModule/Email/index.js
+++ src/containers/MissionPage/Main/MissionModule/Email/index.js
@@ -1,22 +1,10 @@
-import React, { useState, useMemo, useRef } from 'react';
-import Input from 'components/Texts/Input';
-import Button from 'components/Button';
-import Icon from 'components/Icon';
+import React, { useState } from 'react';
 import { Color, mobileMaxWidth } from 'constants/css';
 import { css } from '@emotion/css';
-import { isValidEmail } from 'helpers/stringHelpers';
-import { useAppContext } from 'contexts';
+import EmailSubmitForm from './EmailSubmitForm';
 
 export default function Email() {
-  const {
-    requestHelpers: { sendVerificationOTPEmail }
-  } = useAppContext();
-  const [sendingEmail, setSendingEmail] = useState(false);
-  const sendingEmailRef = useRef(false);
-  const [email, setEmail] = useState('');
   const [emailSent, setEmailSent] = useState(false);
-  const [errorMsg, setErrorMsg] = useState('');
-  const emailIsValid = useMemo(() => isValidEmail(email), [email]);
   return (
     <div style={{ width: '100%' }}>
       <p
@@ -39,71 +27,7 @@ export default function Email() {
           </>
         )}
       </p>
-      {!emailSent && (
-        <div
-          style={{
-            marginTop: '1rem',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%'
-          }}
-        >
-          <Input
-            className={css`
-              width: 50%;
-              @media (max-width: ${mobileMaxWidth}) {
-                width: 100%;
-              }
-            `}
-            type="email"
-            maxLength={100}
-            placeholder="somebody@something.com"
-            onChange={setEmail}
-            value={email}
-          />
-          <div style={{ marginTop: '1.5rem' }}>
-            {errorMsg && <p style={{ color: Color.red() }}>{errorMsg}</p>}
-            <Button
-              disabled={!emailIsValid || sendingEmail}
-              style={{ fontSize: '1.7rem' }}
-              filled
-              color="green"
-              onClick={() => handleConfirmEmail(email)}
-            >
-              {sendingEmail ? (
-                <>
-                  <Icon style={{ marginLeft: '0.7rem' }} icon="spinner" pulse />
-                  <span style={{ marginLeft: '0.7rem' }}>One moment...</span>
-                </>
-              ) : (
-                'Submit'
-              )}
-            </Button>
-          </div>
-        </div>
-      )}
+      {!emailSent && <EmailSubmitForm onSetEmailSent={setEmailSent} />}
     </div>
   );
-
-  async function handleConfirmEmail(email) {
-    if (sendingEmailRef.current) return;
-    try {
-      sendingEmailRef.current = true;
-      setSendingEmail(true);
-      const success = await sendVerificationOTPEmail(email);
-      sendingEmailRef.current = false;
-      setSendingEmail(false);
-      if (success) {
-        setEmailSent(true);
-      } else {
-        setErrorMsg('An error occurred while sending a verification email');
-      }
-    } catch (error) {
-      sendingEmailRef.current = false;
-      setSendingEmail(false);
-      console.error(error);
-    }
-  }
 }
