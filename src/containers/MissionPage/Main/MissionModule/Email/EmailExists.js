dc3b2caee31dd5eb624d218146f6b136ac48ae8c
mikey1384 <mikey1384@gmail.com>
2021-05-03 20:57:06 +0900

    more updates

--- src/containers/MissionPage/Main/MissionModule/Email/EmailExists.js
+++ src/containers/MissionPage/Main/MissionModule/Email/EmailExists.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ErrorBoundary from 'components/ErrorBoundary';
 
 EmailExists.propTypes = {
   emailMissionAttempted: PropTypes.bool
@@ -7,8 +8,20 @@ EmailExists.propTypes = {
 
 export default function EmailExists({ emailMissionAttempted }) {
   return (
-    <div>
-      <div>email exists {emailMissionAttempted}</div>
-    </div>
+    <ErrorBoundary
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <p>
+        {emailMissionAttempted
+          ? 'Congratulations on successfully setting up your own email address'
+          : `Looks like you already have an email address`}
+      </p>
+      <div>email exists</div>
+    </ErrorBoundary>
   );
 }
