526ed08df9f31bd21f263bb422199ba85f1ebfaf
mikey1384 <mikey1384@gmail.com>
2021-05-03 21:39:31 +0900

    minor fix

--- src/containers/MissionPage/Main/MissionModule/Email/EmailExists.js
+++ src/containers/MissionPage/Main/MissionModule/Email/EmailExists.js
@@ -1,17 +1,21 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Color } from 'constants/css';
 import ErrorBoundary from 'components/ErrorBoundary';
+import Button from 'components/Button';
+import Icon from 'components/Icon';
 
 EmailExists.propTypes = {
   emailMissionAttempted: PropTypes.bool
 };
 
 export default function EmailExists({ emailMissionAttempted }) {
+  const [submitDisabled] = useState(false);
   const passMessage = useMemo(
     () =>
       emailMissionAttempted
         ? 'Congratulations on successfully setting up your own email address'
-        : `Looks like you already have an email address`,
+        : `It looks like you already have an email address!`,
     [emailMissionAttempted]
   );
 
@@ -21,11 +25,34 @@ export default function EmailExists({ emailMissionAttempted }) {
         width: '100%',
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center'
+        alignItems: 'center',
+        fontSize: '1.7rem'
       }}
     >
-      <p>{passMessage}</p>
-      <div>email exists</div>
+      <p
+        style={{
+          fontWeight: 'bold',
+          fontSize: '1.8rem',
+          marginBottom: '1.5rem'
+        }}
+      >
+        {passMessage}
+      </p>
+      <p>
+        Press the <b style={{ color: Color.brownOrange() }}>button</b> below to
+        collect your reward
+      </p>
+      <Button
+        filled
+        disabled={submitDisabled}
+        style={{ marginTop: '3.5rem', fontSize: '1.7rem' }}
+        skeuomorphic
+        color="brownOrange"
+        onClick={() => console.log('clicked')}
+      >
+        <Icon icon="bolt" />
+        <span style={{ marginLeft: '1rem' }}>Task Complete</span>
+      </Button>
     </ErrorBoundary>
   );
 }
