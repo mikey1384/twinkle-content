a35239e2d2f7b1175e3f1e05a7be1144cf985ae8
mikey1384 <mikey1384@gmail.com>
2021-05-03 20:06:45 +0900

    more updates

--- src/containers/MissionPage/Main/MissionModule/Email/index.js
+++ src/containers/MissionPage/Main/MissionModule/Email/index.js
@@ -2,18 +2,19 @@ import React, { useMemo } from 'react';
 import { useMyState } from 'helpers/hooks';
 import EmailVerifier from './EmailVerifier';
 import EmailExists from './EmailExists';
+import ErrorBoundary from 'components/ErrorBoundary';
 
 export default function Email() {
   const { verifiedEmail, emailMissionAttempted } = useMyState();
   const conditionPassed = useMemo(() => !!verifiedEmail, [verifiedEmail]);
 
   return (
-    <div style={{ width: '100%' }}>
+    <ErrorBoundary style={{ width: '100%' }}>
       {conditionPassed ? (
         <EmailExists emailMissionAttempted={emailMissionAttempted} />
       ) : (
         <EmailVerifier />
       )}
-    </div>
+    </ErrorBoundary>
   );
 }
