54e6c207ef745aff9341722b9ed10cb92189948a
mikey1384 <mikey1384@gmail.com>
2021-05-06 21:42:13 +0900

    more updates

--- src/containers/MissionPage/Main/MissionModule/GitHub/index.js
+++ src/containers/MissionPage/Main/MissionModule/GitHub/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ErrorBoundary from 'components/ErrorBoundary';
 
 export default function GitHub() {
   return (
-    <div>
+    <ErrorBoundary style={{ width: '100%' }}>
       <div>this is github mission</div>
-    </div>
+    </ErrorBoundary>
   );
 }
