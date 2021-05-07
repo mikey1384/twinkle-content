93e84aa8ed978109b07fca0778c687b2588160fb
mikey1384 <mikey1384@gmail.com>
2021-05-07 17:56:39 +0900

    implement oauth

--- src/containers/MissionPage/Main/MissionModule/GitHub/index.js
+++ src/containers/MissionPage/Main/MissionModule/GitHub/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
 import ErrorBoundary from 'components/ErrorBoundary';
 import Button from 'components/Button';
+import queryString from 'query-string';
 
 export default function GitHub() {
+  const location = useLocation();
+  const { search } = location;
+  const { code } = useMemo(() => queryString.parse(search), [search]);
+  console.log(code);
+
   return (
     <ErrorBoundary style={{ width: '100%' }}>
       <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
@@ -14,6 +21,6 @@ export default function GitHub() {
   );
 
   async function handleGitHubButtonClick() {
-    console.log('this button has been clicked');
+    window.location = `https://github.com/login/oauth/authorize?scope=user&client_id=Iv1.24b0e3180ad6cc23`;
   }
 }
