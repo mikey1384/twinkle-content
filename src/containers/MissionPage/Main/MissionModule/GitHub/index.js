368748d3c19547303b8748ebfa1acf4b4532185d
mikey1384 <mikey1384@gmail.com>
2021-05-07 17:28:00 +0900

    add github button

--- src/containers/MissionPage/Main/MissionModule/GitHub/index.js
+++ src/containers/MissionPage/Main/MissionModule/GitHub/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ErrorBoundary from 'components/ErrorBoundary';
+import Button from 'components/Button';
 
 export default function GitHub() {
   return (
     <ErrorBoundary style={{ width: '100%' }}>
-      <div>this is github mission</div>
+      <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+        <Button filled color="logoBlue" onClick={handleGitHubButtonClick}>
+          GitHub Button
+        </Button>
+      </div>
     </ErrorBoundary>
   );
+
+  async function handleGitHubButtonClick() {
+    console.log('this button has been clicked');
+  }
 }
