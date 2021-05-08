bfb39d1501e70c1caebc6ab938108b9bbf5578ed
mikey1384 <mikey1384@gmail.com>
2021-05-08 16:13:07 +0900

    implement TaskComplete component for GitHub mission

--- src/containers/MissionPage/Main/MissionModule/GitHub/index.js
+++ src/containers/MissionPage/Main/MissionModule/GitHub/index.js
@@ -1,15 +1,28 @@
 import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
 import ErrorBoundary from 'components/ErrorBoundary';
 import GitHubVerifier from './GitHubVerifier';
 import { useMyState } from 'helpers/hooks';
+import TaskComplete from '../components/TaskComplete';
 
-export default function GitHub() {
+GitHub.propTypes = {
+  taskId: PropTypes.number.isRequired
+};
+
+export default function GitHub({ taskId }) {
   const { githubUsername } = useMyState();
   const conditionPassed = useMemo(() => !!githubUsername, [githubUsername]);
 
   return (
     <ErrorBoundary style={{ width: '100%' }}>
-      {conditionPassed ? <div>Github exists</div> : <GitHubVerifier />}
+      {conditionPassed ? (
+        <TaskComplete
+          taskId={taskId}
+          passMessage="Great job creating your GitHub account!"
+        />
+      ) : (
+        <GitHubVerifier />
+      )}
     </ErrorBoundary>
   );
 }
