fd49eab0be322ebde360d04cb85e75e061a0c266
mikey1384 <mikey1384@gmail.com>
2021-05-08 09:20:34 +0900

    more updates

--- src/containers/MissionPage/Main/MissionModule/GitHub/index.js
+++ src/containers/MissionPage/Main/MissionModule/GitHub/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import ErrorBoundary from 'components/ErrorBoundary';
 import GitHubVerifier from './GitHubVerifier';
+import { useMyState } from 'helpers/hooks';
 
 export default function GitHub() {
+  const { githubUsername } = useMyState();
+  const conditionPassed = useMemo(() => !!githubUsername, [githubUsername]);
+
   return (
     <ErrorBoundary style={{ width: '100%' }}>
-      <GitHubVerifier />
+      {conditionPassed ? <div>Github exists</div> : <GitHubVerifier />}
     </ErrorBoundary>
   );
 }
