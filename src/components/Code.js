195221577496b3260b5390868e930707b4491f09
mikey1384 <mikey1384@gmail.com>
2021-05-10 18:10:22 +0900

    more updates

--- src/components/Code.js
+++ src/components/Code.js
@@ -3,7 +3,17 @@ import PropTypes from 'prop-types';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import { Color } from 'constants/css';
 import dracula from 'prism-react-renderer/themes/dracula';
+import duotoneDark from 'prism-react-renderer/themes/duotoneDark';
+import duotoneLight from 'prism-react-renderer/themes/duotoneLight';
+import nightOwl from 'prism-react-renderer/themes/nightOwl';
+import nightOwlLight from 'prism-react-renderer/themes/nightOwlLight';
 import github from 'prism-react-renderer/themes/github';
+import oceanicNext from 'prism-react-renderer/themes/oceanicNext';
+import okaidia from 'prism-react-renderer/themes/okaidia';
+import palenight from 'prism-react-renderer/themes/palenight';
+import shadesOfPurple from 'prism-react-renderer/themes/shadesOfPurple';
+import synthwave84 from 'prism-react-renderer/themes/synthwave84';
+import ultramin from 'prism-react-renderer/themes/ultramin';
 import vsDark from 'prism-react-renderer/themes/vsDark';
 import vsLight from 'prism-react-renderer/themes/vsLight';
 
@@ -17,7 +27,17 @@ Code.propTypes = {
 
 const availableThemes = {
   dracula,
+  duotoneDark,
+  duotoneLight,
   github,
+  nightOwl,
+  nightOwlLight,
+  oceanicNext,
+  okaidia,
+  palenight,
+  shadesOfPurple,
+  synthwave84,
+  ultramin,
   vsDark,
   vsLight
 };
@@ -29,7 +49,7 @@ export default function Code({
   style,
   theme
 }) {
-  const selectedTheme = useMemo(() => availableThemes[theme] || vsDark, [
+  const selectedTheme = useMemo(() => availableThemes?.[theme] || okaidia, [
     theme
   ]);
 
