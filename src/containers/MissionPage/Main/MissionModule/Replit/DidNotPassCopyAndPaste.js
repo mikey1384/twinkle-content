b389c23d247d8b6ca84fcd6f541c044ce2926892
mikey1384 <mikey1384@gmail.com>
2021-05-13 15:00:08 +0900

    minor fix

--- src/containers/MissionPage/Main/MissionModule/Replit/DidNotPassCopyAndPaste.js
+++ src/containers/MissionPage/Main/MissionModule/Replit/DidNotPassCopyAndPaste.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ErrorBoundary from 'components/ErrorBoundary';
+import { css } from '@emotion/css';
 
 export default function DidNotPassCopyAndPaste() {
   return (
-    <ErrorBoundary>
-      <div>Did not pass copy and paste yet</div>
+    <ErrorBoundary
+      className={css`
+        width: 100%;
+        font-size: 1.5rem;
+        text-align: center;
+        > p {
+          font-weight: bold;
+          font-size: 1.7rem;
+        }
+      `}
+    >
+      <h3>You did not pass the Copy and Paste mission, yet</h3>
     </ErrorBoundary>
   );
 }
