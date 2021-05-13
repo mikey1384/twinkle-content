07d32f86a51bde2788f3540575b5419ff14e573f
mikey1384 <mikey1384@gmail.com>
2021-05-13 22:59:06 +0900

    more updates

--- src/containers/MissionPage/Main/MissionModule/Replit/ReplitVerifier/CreateNewRepl.js
+++ src/containers/MissionPage/Main/MissionModule/Replit/ReplitVerifier/CreateNewRepl.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'components/Button';
+import Icon from 'components/Icon';
+import { Color, mobileMaxWidth } from 'constants/css';
 import { css } from '@emotion/css';
 
 CreateNewRepl.propTypes = {
@@ -8,18 +10,22 @@ CreateNewRepl.propTypes = {
 };
 
 export default function CreateNewRepl({ style }) {
+  const [helpButtonPressed, setHelpButtonPressed] = useState(false);
   return (
     <div
       style={style}
       className={css`
         width: 100%;
-        font-size: 1.5rem;
+        font-size: 1.7rem;
         display: flex;
         flex-direction: column;
         align-items: center;
         > p {
           font-size: 2rem;
           font-weight: bold;
+        }
+        @media (max-width: ${mobileMaxWidth}) {
+          font-size: 1.5rem;
         }
       `}
     >
@@ -36,14 +42,22 @@ export default function CreateNewRepl({ style }) {
         <Button filled color="green" onClick={() => console.log('yes')}>
           I created it
         </Button>
-        <Button
-          style={{ marginTop: '1rem' }}
-          filled
-          color="logoBlue"
-          onClick={() => console.log('yes')}
-        >
-          {`I don't understand what I am supposed to do`}
-        </Button>
+        {!helpButtonPressed && (
+          <Button
+            style={{ marginTop: '1rem' }}
+            filled
+            color="logoBlue"
+            onClick={() => setHelpButtonPressed(true)}
+          >
+            {`I don't understand what I am supposed to do`}
+          </Button>
+        )}
+        {helpButtonPressed && (
+          <div style={{ marginTop: '2.5rem', marginBottom: '-1rem' }}>
+            Read the <b style={{ color: Color.green() }}>tutorial</b> below{' '}
+            <Icon icon="arrow-down" /> to learn how to create a Next.js Repl
+          </div>
+        )}
       </div>
     </div>
   );
