cf80e09d7cdbbc7b4fb068f853895e3461eda6bf
mikey1384 <mikey1384@gmail.com>
2021-05-14 15:56:48 +0900

    more updates

--- src/containers/MissionPage/Main/MissionModule/Replit/ReplitVerifier/CreateNewRepl.js
+++ src/containers/MissionPage/Main/MissionModule/Replit/ReplitVerifier/CreateNewRepl.js
@@ -6,11 +6,14 @@ import { Color, mobileMaxWidth } from 'constants/css';
 import { css } from '@emotion/css';
 
 CreateNewRepl.propTypes = {
+  replCreated: PropTypes.bool.isRequired,
+  onCreateRepl: PropTypes.func.isRequired,
   style: PropTypes.object
 };
 
-export default function CreateNewRepl({ style }) {
+export default function CreateNewRepl({ replCreated, onCreateRepl, style }) {
   const [helpButtonPressed, setHelpButtonPressed] = useState(false);
+
   return (
     <div
       style={style}
@@ -29,36 +32,49 @@ export default function CreateNewRepl({ style }) {
         }
       `}
     >
-      <p>2. Great job! Now, create a Next.js Repl</p>
-      <div
-        style={{
-          marginTop: '2.5rem',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column'
-        }}
-      >
-        <Button filled color="green" onClick={() => console.log('yes')}>
-          I created it
-        </Button>
-        {!helpButtonPressed && (
-          <Button
-            style={{ marginTop: '1rem' }}
-            filled
-            color="logoBlue"
-            onClick={() => setHelpButtonPressed(true)}
+      <p>
+        2. Great job! Now, create a Next.js Repl
+        {replCreated && (
+          <Icon
+            style={{ marginLeft: '1rem' }}
+            color={Color.green()}
+            icon="check"
+          />
+        )}
+      </p>
+      {!replCreated && (
+        <>
+          <div
+            style={{
+              marginTop: '2.5rem',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column'
+            }}
           >
-            {`I don't understand what I am supposed to do`}
-          </Button>
-        )}
-        {helpButtonPressed && (
-          <div style={{ marginTop: '3rem', marginBottom: '-1rem' }}>
-            Read the <b style={{ color: Color.green() }}>tutorial</b> below{' '}
-            <Icon icon="arrow-down" /> to learn how to create a Next.js Repl
+            <Button filled color="green" onClick={onCreateRepl}>
+              I created it
+            </Button>
+            {!helpButtonPressed && (
+              <Button
+                style={{ marginTop: '1rem' }}
+                filled
+                color="orange"
+                onClick={() => setHelpButtonPressed(true)}
+              >
+                {`I don't understand what I am supposed to do`}
+              </Button>
+            )}
+            {helpButtonPressed && (
+              <div style={{ marginTop: '3rem', marginBottom: '-1rem' }}>
+                Read the <b style={{ color: Color.green() }}>tutorial</b> below{' '}
+                <Icon icon="arrow-down" /> to learn how to create a Next.js Repl
+              </div>
+            )}
           </div>
-        )}
-      </div>
+        </>
+      )}
     </div>
   );
 }
--- src/containers/MissionPage/Main/MissionModule/Replit/ReplitVerifier/index.js
+++ src/containers/MissionPage/Main/MissionModule/Replit/ReplitVerifier/index.js
@@ -5,13 +5,20 @@ import CreateNewRepl from './CreateNewRepl';
 
 export default function ReplitVerifier() {
   const [accountMade, setAccountMade] = useState(false);
+  const [replCreated, setReplCreated] = useState(false);
   return (
     <ErrorBoundary style={{ width: '100%', marginTop: '1rem' }}>
       <MakeAccount
         accountMade={accountMade}
         onMakeAccount={() => setAccountMade(true)}
       />
-      {accountMade && <CreateNewRepl style={{ marginTop: '15rem' }} />}
+      {accountMade && (
+        <CreateNewRepl
+          style={{ marginTop: replCreated ? '2rem' : '15rem' }}
+          replCreated={replCreated}
+          onCreateRepl={() => setReplCreated(true)}
+        />
+      )}
     </ErrorBoundary>
   );
 }
