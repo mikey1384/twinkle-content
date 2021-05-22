fb3005795facf6c92a27bb9e1dbab6e20efa4105
mikey1384 <mikey1384@gmail.com>
2021-05-22 18:42:53 +0900

    minor fix

--- src/containers/MissionPage/Main/MissionModule/HelloWorld/CodingHelloWorld/index.js
+++ src/containers/MissionPage/Main/MissionModule/HelloWorld/CodingHelloWorld/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ErrorBoundary from 'components/ErrorBoundary';
-import CodeSandBox from './CodeSandBox';
+import CodeSandbox from './CodeSandbox';
 
 CodingHelloWorld.propTypes = {
   style: PropTypes.object
@@ -11,7 +11,7 @@ export default function CodingHelloWorld({ style }) {
   return (
     <ErrorBoundary style={style}>
       <p>{`2. Let's code your first Hello World`}</p>
-      <CodeSandBox />
+      <CodeSandbox />
     </ErrorBoundary>
   );
 }
