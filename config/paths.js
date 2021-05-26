9ec0f663443349588fbd819c16dca6ae5d2bc35b
mikey1384 <mikey1384@gmail.com>
2021-05-26 22:19:08 +0900

    more fixes

--- config/paths.js
+++ config/paths.js
@@ -54,7 +54,6 @@ module.exports = {
   appIndexJs: resolveModule(resolveApp, 'src/index'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
-  appTsConfig: resolveApp('tsconfig.json'),
   appJsConfig: resolveApp('jsconfig.json'),
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveModule(resolveApp, 'src/setupTests'),
