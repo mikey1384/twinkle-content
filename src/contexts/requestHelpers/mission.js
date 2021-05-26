b000237621680c555e8c6ad2a83cba628fcd5c71
mikey1384 <mikey1384@gmail.com>
2021-05-26 19:58:27 +0900

    remove unused codemirror package

--- src/contexts/requestHelpers/mission.js
+++ src/contexts/requestHelpers/mission.js
@@ -83,18 +83,6 @@ export default function missionRequestHelpers({ auth, handleError }) {
         return handleError(error);
       }
     },
-    async lintCode(code) {
-      try {
-        const {
-          data: { results }
-        } = await request.post(`${URL}/mission/code/lint`, {
-          code
-        });
-        return Promise.resolve(results);
-      } catch (error) {
-        return handleError(error);
-      }
-    },
     async loadGitHubData(code) {
       try {
         const {
