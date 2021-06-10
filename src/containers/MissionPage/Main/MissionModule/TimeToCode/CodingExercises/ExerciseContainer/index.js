2f265ae287808332ba32028c2ce142e7b3246619
mikey1384 <mikey1384@gmail.com>
2021-06-10 23:17:13 +0900

    implement autoscroll to next exercise upon exercise completion

--- src/containers/MissionPage/Main/MissionModule/TimeToCode/CodingExercises/ExerciseContainer/index.js
+++ src/containers/MissionPage/Main/MissionModule/TimeToCode/CodingExercises/ExerciseContainer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import ErrorBoundary from 'components/ErrorBoundary';
 import SuccessMessage from './SuccessMessage';
@@ -7,6 +7,7 @@ import Icon from 'components/Icon';
 import CodeSandbox from 'components/Forms/CodeSandbox';
 import { Color, mobileMaxWidth } from 'constants/css';
 import { css } from '@emotion/css';
+import { scrollElementToCenter } from 'helpers';
 import { useMyState } from 'helpers/hooks';
 import { useAppContext, useContentContext } from 'contexts';
 import useExercises from './useExercises';
@@ -44,63 +45,73 @@ export default function ExerciseContainer({
       updateMissionStatus,
       userId
     });
+  const ComponentRef = useRef(null);
+  const prevPassedRef = useRef(prevPassed);
+  useEffect(() => {
+    if (!prevPassedRef.current && prevPassed) {
+      scrollElementToCenter(ComponentRef.current, -100);
+    }
+  }, [exerciseKey, prevPassed]);
 
   return prevPassed ? (
-    <ErrorBoundary
-      style={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        ...style
-      }}
-    >
-      <p>
-        {exercise.title}
-        {passed && (
-          <Icon
-            style={{ marginLeft: '1rem' }}
-            icon="check"
-            color={Color.green()}
+    <ErrorBoundary>
+      <div
+        ref={ComponentRef}
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          ...style
+        }}
+      >
+        <p>
+          {exercise.title}
+          {passed && (
+            <Icon
+              style={{ marginLeft: '1rem' }}
+              icon="check"
+              color={Color.green()}
+            />
+          )}
+        </p>
+        <div
+          className={css`
+            width: 80%;
+            font-size: 1.7rem;
+            line-height: 2;
+            text-align: center;
+            @media (max-width: ${mobileMaxWidth}) {
+              width: 100%;
+            }
+          `}
+          style={{ marginTop: '2rem' }}
+        >
+          {exercise.instruction}
+        </div>
+        <div
+          className={css`
+            margin-top: 2rem;
+            width: 80%;
+            @media (max-width: ${mobileMaxWidth}) {
+              width: 100%;
+            }
+          `}
+        >
+          <CodeSandbox
+            code={exercise.code || exercise.initialCode}
+            onSetCode={exercise.onSetCode}
+            onRunCode={exercise.onRunCode}
+            onSetErrorMsg={setErrorMsg}
+            hasError={!!errorMsg}
+            passed={passed || success}
+            runButtonLabel="check"
           />
-        )}
-      </p>
-      <div
-        className={css`
-          width: 80%;
-          font-size: 1.7rem;
-          line-height: 2;
-          text-align: center;
-          @media (max-width: ${mobileMaxWidth}) {
-            width: 100%;
-          }
-        `}
-        style={{ marginTop: '2rem' }}
-      >
-        {exercise.instruction}
-      </div>
-      <div
-        className={css`
-          margin-top: 2rem;
-          width: 80%;
-          @media (max-width: ${mobileMaxWidth}) {
-            width: 100%;
-          }
-        `}
-      >
-        <CodeSandbox
-          code={exercise.code || exercise.initialCode}
-          onSetCode={exercise.onSetCode}
-          onRunCode={exercise.onRunCode}
-          onSetErrorMsg={setErrorMsg}
-          hasError={!!errorMsg}
-          passed={passed || success}
-          runButtonLabel="check"
-        />
-        {success && !passed && (
-          <SuccessMessage onNextClick={exercise.onNextClick} />
-        )}
-        {errorMsg && <FailMessage message={errorMsg} />}
+          {success && !passed && (
+            <SuccessMessage onNextClick={exercise.onNextClick} />
+          )}
+          {errorMsg && <FailMessage message={errorMsg} />}
+        </div>
       </div>
     </ErrorBoundary>
   ) : null;
