be42b7376e340f9c1963a4db9a0df68819a5bb6a
mikey1384 <mikey1384@gmail.com>
2021-06-12 10:41:21 +0900

    add changing-font exercise

---
+++ src/containers/MissionPage/Main/MissionModule/TimeToCode/CodingExercises/exercises/changingFont.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { WELCOME_MSG } from './constants';
+import { getAstProps } from 'helpers';
+import { stringIsEmpty } from 'helpers/stringHelpers';
+
+export const title = `5. Changing the font`;
+export const instruction = (
+  <>
+    Type a message that says <b>{WELCOME_MSG}</b> between <b>{`<p>`}</b> and{' '}
+    <b>{`</p>`}</b>
+  </>
+);
+export const initialCode = `function HomePage() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      }}
+    >
+      <p></p>
+      <button
+        style={{
+          color: "white",
+          background: "blue",
+          border: "none",
+          fontSize: "2rem",
+          padding: "1rem",
+          cursor: "pointer"
+        }}
+        onClick={() => alert('Hello World')}
+      >
+        Tap me
+      </button>
+    </div>
+  );
+}`;
+
+export async function onRunCode({ ast, onSetErrorMsg, onUpdateMissionStatus }) {
+  const jsxElements = getAstProps({
+    ast,
+    propType: 'JSXElement'
+  });
+  let welcomeText = '';
+  for (let element of jsxElements) {
+    if (element.openingElement?.name?.name === 'p' && element?.children) {
+      for (let child of element?.children) {
+        welcomeText = child?.value || '';
+      }
+    }
+  }
+  if (
+    welcomeText.trim().toLowerCase() === 'Welcome to my website!'.toLowerCase()
+  ) {
+    return await onUpdateMissionStatus();
+  }
+  if (stringIsEmpty(welcomeText)) {
+    return onSetErrorMsg(
+      `Hmmm... There doesn't seem to be any message between <p> and </p>`
+    );
+  }
+  if (
+    welcomeText.trim().toLowerCase() === 'Welcome to my website'.toLowerCase()
+  ) {
+    return onSetErrorMsg(
+      `You forgot to add an exclamation mark (!) at the end`
+    );
+  }
+  onSetErrorMsg(
+    `The alert message should say, "Hello world," not "${welcomeText.trim()}"`
+  );
+}
--- src/containers/MissionPage/Main/MissionModule/TimeToCode/CodingExercises/exercises/index.js
+++ src/containers/MissionPage/Main/MissionModule/TimeToCode/CodingExercises/exercises/index.js
@@ -2,10 +2,12 @@ import * as changeButtonColor from './changeButtonColor';
 import * as changeButtonLabel from './changeButtonLabel';
 import * as changeAlertMsg from './changeAlertMsg';
 import * as welcomeMsg from './welcomeMsg';
+import * as changingFont from './changingFont';
 
 export default {
   changeButtonColor,
   changeButtonLabel,
   changeAlertMsg,
-  welcomeMsg
+  welcomeMsg,
+  changingFont
 };
