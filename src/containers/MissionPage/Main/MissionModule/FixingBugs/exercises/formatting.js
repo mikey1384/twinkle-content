d0b18649943ddc2edd3178ef0630f5e5acc6c49f
mikey1384 <mikey1384@gmail.com>
2021-06-15 21:26:28 +0900

    more fix

--- src/containers/MissionPage/Main/MissionModule/FixingBugs/exercises/formatting.js
+++ src/containers/MissionPage/Main/MissionModule/FixingBugs/exercises/formatting.js
@@ -9,8 +9,7 @@ export const instruction = (
   </>
 );
 export const initialCode = `function HomePage() {
-  return (<div style={{width: "100%",height: "100%",display: "flex",flexDirection: "column", alignItems: "center"}}><p style={{color: "#FF00FF",fontFamily: "sans-serif",fontWeight: "bold",fontSize: "2rem"}}>Welcome to My Website!</p><button style={{marginTop: "3rem",padding: "1rem",color: "white",background: "blue",border: "none",fontSize: "2rem",cursor: "pointer"}} onClick={() => alert("Hello World")}>Tap me</button></div>);
-}`;
+  return (<div style={{width: "100%",height: "100%",display: "flex",flexDirection: "column", alignItems: "center"}}><p style={{color: "#FF00FF",fontFamily: "sans-serif",fontWeight: "bold",fontSize: "2rem"}}>Welcome to My Website!</p><button style={{marginTop: "3rem",padding: "1rem",color: "white",background: "blue",border: "none",fontSize: "2rem",cursor: "pointer"}} onClick={() => alert("Hello World")}>Tap me</button></div>);}`;
 
 const formattedCode = `function HomePage() {
   return (
