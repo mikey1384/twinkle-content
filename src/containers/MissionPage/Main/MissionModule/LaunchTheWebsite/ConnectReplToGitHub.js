6507e857254c8c28231088458c34376ea9b566b0
mikey1384 <mikey1384@gmail.com>
2021-07-08 21:57:47 +0900

    more fix

--- src/containers/MissionPage/Main/MissionModule/LaunchTheWebsite/ConnectReplToGitHub.js
+++ src/containers/MissionPage/Main/MissionModule/LaunchTheWebsite/ConnectReplToGitHub.js
@@ -11,14 +11,19 @@ ConnectReplToGitHub.propTypes = {
 export default function ConnectReplToGitHub({ index, okayPressed }) {
   return (
     <StepSlide title="Connect your Repl to GitHub" index={index}>
-      <div style={{ marginBottom: '5rem', textAlign: 'center' }}>
+      <div
+        style={{
+          marginBottom: okayPressed ? '2rem' : '2.5rem',
+          textAlign: 'center'
+        }}
+      >
         <p>
           Follow the instructions in the{' '}
           <b style={{ color: Color.green() }}>tutorial</b> to connect your Repl
           to your GitHub
         </p>
         {okayPressed && (
-          <div style={{ marginTop: '5rem' }}>
+          <div style={{ marginTop: '4rem' }}>
             <p>Were you able to connect your Repl to your GitHub?</p>
           </div>
         )}
