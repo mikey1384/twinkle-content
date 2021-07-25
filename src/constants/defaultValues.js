39617a0689307a35f840492b79aa5f7fa8fbc38e
mikey1384 <mikey1384@gmail.com>
2021-07-25 23:22:16 +0900

    more bug fix

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,4 +1,4 @@
-export const clientVersion = '1.5.10';
+export const clientVersion = '1.5.11';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
