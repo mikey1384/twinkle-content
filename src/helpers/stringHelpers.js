ce279e304677911a854a006686d3200b1d1897e2
mikey1384 <mikey1384@gmail.com>
2021-08-27 06:36:07 +0900

    minor fix

--- src/helpers/stringHelpers.js
+++ src/helpers/stringHelpers.js
@@ -533,7 +533,7 @@ export function processedStringWithURL(string) {
       /((\*\*(?![0-9])[^\s]){1}((?!(\*\*))[^\n])+([^\s]\*\*(?![0-9])){1})/gi;
     const underlineWordRegex = /(__[^\s]+__)/gi;
     const underlineSentenceRegex = /(__[^\s]){1}((?!(__))[^\n])+([^\s]__){1}/gi;
-    const linethroughWordRegex = /(--[^\s]+--)/gi;
+    const linethroughWordRegex = /(--[^\s-]+--)/gi;
     const lineThroughSentenceRegex =
       /(--[^\s-]){1}((?!(-))[^\n])+([^\s-]--){1}/gi;
 
