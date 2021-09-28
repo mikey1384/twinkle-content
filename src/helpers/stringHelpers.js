f504f90859f0d7d17d9fefb5083053e073a4d85f
mikey1384 <mikey1384@gmail.com>
2021-09-28 14:38:35 +0900

    more fix

--- src/helpers/stringHelpers.js
+++ src/helpers/stringHelpers.js
@@ -537,7 +537,7 @@ export function processedStringWithURL(string) {
     const lineThroughSentenceRegex =
       /(--[^\s-]){1}((?!(-))[^\n])+([^\s-]--){1}/gi;
     const fakeAtSymbolRegex = /＠/gi;
-    const mentionRegex = /((?![^\s])[\S\s]{1}|^.?)(@[^\s.,`'":?!]+)/g;
+    const mentionRegex = /((?![^\S\s])[\S\s]{0}|^.?)(@[^\s.,`'":?!@]+)/g;
 
     return string
       .replace(/(<br>)/gi, '\n')
