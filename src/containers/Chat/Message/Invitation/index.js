45b7bf24c143acbbad6323539958eaa13a83a6e0
mikey1384 <mikey1384@gmail.com>
2021-10-21 09:16:25 +0900

    more updates

--- src/containers/Chat/Message/Invitation/index.js
+++ src/containers/Chat/Message/Invitation/index.js
@@ -57,7 +57,7 @@ export default function Invitation({
       });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [invitationChannelId]);
 
   const invitationChannel = useMemo(
     () => channelsObj[invitationChannelId],
