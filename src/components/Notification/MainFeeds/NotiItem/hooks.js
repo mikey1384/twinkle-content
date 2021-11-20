c7457394c145db09aaad6a1c673fdbeec3b0ea8c
mikey1384 <mikey1384@gmail.com>
2021-11-20 17:26:13 +0900

    add localization hook file for Notification component

--- src/components/Notification/MainFeeds/NotiItem/hooks.js
+++ src/components/Notification/MainFeeds/NotiItem/hooks.js
@@ -1,35 +1,21 @@
 import React, { useMemo } from 'react';
-import PropTypes from 'prop-types';
-import UsernameText from 'components/Texts/UsernameText';
-import ErrorBoundary from 'components/ErrorBoundary';
 import ContentLink from 'components/ContentLink';
-import { timeSince } from 'helpers/timeStampHelpers';
-import { Color } from 'constants/css';
-import { truncateText, stringIsEmpty } from 'helpers/stringHelpers';
 import { useMyState } from 'helpers/hooks';
-import { notiFeedListItem } from '../Styles';
+import { stringIsEmpty, truncateText } from 'helpers/stringHelpers';
+import { Color } from 'constants/css';
 
-NotiItem.propTypes = {
-  notification: PropTypes.object.isRequired
-};
-
-export default function NotiItem({
-  notification: {
-    id,
-    actionObj = {},
-    targetComment = {},
-    targetObj = {},
-    targetSubject = {},
-    timeStamp,
-    user = {},
-    rewardType,
-    rewardRootId,
-    rewardRootType,
-    rewardRootMissionType,
-    isNotification,
-    isTask,
-    rootMissionType
-  }
+export function useEnglish({
+  actionObj,
+  isNotification,
+  isTask,
+  rewardRootId,
+  rewardType,
+  rewardRootMissionType,
+  rewardRootType,
+  rootMissionType,
+  targetObj,
+  targetComment,
+  targetSubject
 }) {
   const { userId } = useMyState();
   const contentPreview = useMemo(() => {
@@ -336,21 +322,5 @@ export default function NotiItem({
     userId
   ]);
 
-  return (
-    <ErrorBoundary>
-      <nav style={{ background: '#fff' }} className={notiFeedListItem} key={id}>
-        <div>
-          {actionObj.contentType !== 'pass' &&
-            actionObj.contentType !== 'fail' && (
-              <>
-                <UsernameText user={user} color={Color.blue()} />
-                &nbsp;
-              </>
-            )}
-          {NotificationMessage}
-        </div>
-        <small style={{ color: Color.gray() }}>{timeSince(timeStamp)}</small>
-      </nav>
-    </ErrorBoundary>
-  );
+  return NotificationMessage;
 }
---
+++ src/components/Notification/MainFeeds/NotiItem/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import UsernameText from 'components/Texts/UsernameText';
+import ErrorBoundary from 'components/ErrorBoundary';
+import { timeSince } from 'helpers/timeStampHelpers';
+import { Color } from 'constants/css';
+import { notiFeedListItem } from '../../Styles';
+import { useEnglish } from './hooks';
+
+NotiItem.propTypes = {
+  notification: PropTypes.object.isRequired
+};
+
+export default function NotiItem({
+  notification: {
+    id,
+    actionObj = {},
+    targetComment = {},
+    targetObj = {},
+    targetSubject = {},
+    timeStamp,
+    user = {},
+    rewardType,
+    rewardRootId,
+    rewardRootType,
+    rewardRootMissionType,
+    isNotification,
+    isTask,
+    rootMissionType
+  }
+}) {
+  const EnglishNotificationMessage = useEnglish({
+    actionObj,
+    isNotification,
+    isTask,
+    rewardRootId,
+    rewardType,
+    rewardRootMissionType,
+    rewardRootType,
+    rootMissionType,
+    targetObj,
+    targetComment,
+    targetSubject
+  });
+
+  return (
+    <ErrorBoundary>
+      <nav style={{ background: '#fff' }} className={notiFeedListItem} key={id}>
+        <div>
+          {actionObj.contentType !== 'pass' &&
+            actionObj.contentType !== 'fail' && (
+              <>
+                <UsernameText user={user} color={Color.blue()} />
+                &nbsp;
+              </>
+            )}
+          {EnglishNotificationMessage}
+        </div>
+        <small style={{ color: Color.gray() }}>{timeSince(timeStamp)}</small>
+      </nav>
+    </ErrorBoundary>
+  );
+}
