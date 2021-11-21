3098b0993c923089bcf629921461fcbebdee51d1
mikey1384 <mikey1384@gmail.com>
2021-11-21 16:21:18 +0900

    more refactoring

---
+++ src/components/ContentPanel/Heading/useHeadingText/index.js
@@ -0,0 +1,62 @@
+import { useMemo } from 'react';
+import { useMyState } from 'helpers/hooks';
+import renderEnglishMessage from './localize/english';
+import renderKoreanMessage from './localize/korean';
+
+const selectedLanguage = process.env.REACT_APP_SELECTED_LANGUAGE;
+
+export default function useHeadingText({
+  actionObj,
+  targetObj,
+  targetComment,
+  targetSubject,
+  isNotification,
+  isTask,
+  rewardRootId,
+  rewardType,
+  rewardRootMissionType,
+  rewardRootType,
+  rootMissionType,
+  user
+}) {
+  const { userId } = useMyState();
+  const NotificationMessage = useMemo(() => {
+    const isReply = targetComment?.userId === userId;
+    const isSubjectResponse = targetSubject?.userId === userId;
+    const params = {
+      actionObj,
+      isNotification,
+      isReply,
+      isSubjectResponse,
+      isTask,
+      rewardRootId,
+      rewardType,
+      rewardRootMissionType,
+      rewardRootType,
+      rootMissionType,
+      targetComment,
+      targetObj,
+      targetSubject,
+      user
+    };
+    return selectedLanguage === 'en'
+      ? renderEnglishMessage(params)
+      : renderKoreanMessage(params);
+  }, [
+    actionObj,
+    isNotification,
+    isTask,
+    rewardRootId,
+    rewardRootMissionType,
+    rewardRootType,
+    rewardType,
+    rootMissionType,
+    targetComment,
+    targetObj,
+    targetSubject,
+    user,
+    userId
+  ]);
+
+  return NotificationMessage;
+}
