fa80bb8dff6ba3280a8a225d22318f2246356cb9
mikey1384 <mikey1384@gmail.com>
2021-11-22 21:37:56 +0900

    more refactoring

--- src/components/Notification/MainFeeds/RewardItem.js
+++ src/components/Notification/MainFeeds/RewardItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import ContentLink from 'components/ContentLink';
 import UsernameText from 'components/Texts/UsernameText';
@@ -28,57 +28,57 @@ export default function RewardItem({
     timeStamp
   }
 }) {
-  let notiText = '';
-  if (rewardType === 'Twinkle') {
-    notiText = (
-      <div>
-        <UsernameText
-          user={{ id: rewarderId, username: rewarderUsername }}
-          color={Color.blue()}
-        />{' '}
-        <span
-          style={{
-            color:
-              rewardAmount >= 10
-                ? Color.gold()
-                : rewardAmount >= 5
-                ? Color.rose()
-                : rewardAmount >= 3
-                ? Color.pink()
-                : Color.lightBlue(),
-            fontWeight: 'bold'
-          }}
-        >
-          rewarded you {rewardAmount === 1 ? 'a' : rewardAmount} {rewardType}
-          {rewardAmount > 1 ? 's' : ''}
-        </span>{' '}
-        for your{' '}
-        <ContentLink
-          style={{ color: Color.green() }}
-          content={{
-            id: contentId,
-            title: `${contentType}${
-              !targetObj ||
-              targetObj.notFound ||
-              (contentType === 'comment' && targetObj?.filePath)
-                ? ''
-                : contentType === 'comment'
-                ? ` (${truncateText({
-                    text: targetObj.content,
-                    limit: 100
-                  })})`
-                : ` (${truncateText({
-                    text: targetObj.title,
-                    limit: 100
-                  })})`
-            }`
-          }}
-          contentType={contentType}
-        />
-      </div>
-    );
-  } else {
-    notiText = (
+  const NotiText = useMemo(() => {
+    if (rewardType === 'Twinkle') {
+      return (
+        <div>
+          <UsernameText
+            user={{ id: rewarderId, username: rewarderUsername }}
+            color={Color.blue()}
+          />{' '}
+          <span
+            style={{
+              color:
+                rewardAmount >= 10
+                  ? Color.gold()
+                  : rewardAmount >= 5
+                  ? Color.rose()
+                  : rewardAmount >= 3
+                  ? Color.pink()
+                  : Color.lightBlue(),
+              fontWeight: 'bold'
+            }}
+          >
+            rewarded you {rewardAmount === 1 ? 'a' : rewardAmount} {rewardType}
+            {rewardAmount > 1 ? 's' : ''}
+          </span>{' '}
+          for your{' '}
+          <ContentLink
+            style={{ color: Color.green() }}
+            content={{
+              id: contentId,
+              title: `${contentType}${
+                !targetObj ||
+                targetObj.notFound ||
+                (contentType === 'comment' && targetObj?.filePath)
+                  ? ''
+                  : contentType === 'comment'
+                  ? ` (${truncateText({
+                      text: targetObj.content,
+                      limit: 100
+                    })})`
+                  : ` (${truncateText({
+                      text: targetObj.title,
+                      limit: 100
+                    })})`
+              }`
+            }}
+            contentType={contentType}
+          />
+        </div>
+      );
+    }
+    return (
       <div>
         <UsernameText
           user={{ id: rewarderId, username: rewarderUsername }}
@@ -102,11 +102,23 @@ export default function RewardItem({
         </p>
       </div>
     );
-  }
+  }, [
+    contentId,
+    contentType,
+    isTask,
+    rewardAmount,
+    rewardType,
+    rewarderId,
+    rewarderUsername,
+    rootId,
+    rootMissionType,
+    rootType,
+    targetObj
+  ]);
 
   return (
     <nav style={{ background: '#fff' }} className={notiFeedListItem} key={id}>
-      {notiText}
+      {NotiText}
       <small>{timeSince(timeStamp)}</small>
     </nav>
   );
