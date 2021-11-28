97b87810453ba528cec167450bdd36674051430c
mikey1384 <mikey1384@gmail.com>
2021-11-28 20:06:40 +0900

    more updates

--- src/containers/Explore/Search/SearchBox.js
+++ src/containers/Explore/Search/SearchBox.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import SearchInput from 'components/Texts/SearchInput';
 import { useMyState } from 'helpers/hooks';
 import { useExploreContext } from 'contexts';
+import localize from 'constants/localize';
+
+const selectedLanguage = process.env.REACT_APP_SELECTED_LANGUAGE;
 
 SearchBox.propTypes = {
   category: PropTypes.string,
@@ -19,6 +22,13 @@ export default function SearchBox({ category, className, innerRef, style }) {
     },
     actions: { onChangeSearchInput }
   } = useExploreContext();
+  const placeholderLabel = useMemo(() => {
+    return selectedLanguage === 'en'
+      ? `Search ${category}...`
+      : `${localize(category.slice(0, -1))}${
+          category === 'videos' ? '을' : '를'
+        } 검색하세요...`;
+  }, [category]);
 
   return (
     <SearchInput
@@ -27,7 +37,7 @@ export default function SearchBox({ category, className, innerRef, style }) {
       addonColor={profileTheme}
       borderColor={profileTheme}
       innerRef={innerRef}
-      placeholder={`Search ${category}...`}
+      placeholder={placeholderLabel}
       onChange={onChangeSearchInput}
       value={searchText}
     />
