c69044e54213fc836dcac88198e0345a64bd5962
mikey1384 <mikey1384@gmail.com>
2022-01-05 10:22:49 +0900

    make AlertModal on RewardLevelModal have modalOverModal prop

--- src/components/Modals/RewardLevelModal.js
+++ src/components/Modals/RewardLevelModal.js
@@ -86,6 +86,7 @@ export default function RewardLevelModal({
       </ErrorBoundary>
       {cannotChangeModalShown && (
         <AlertModal
+          modalOverModal
           title={settingCannotBeChangedLabel}
           content={moderatorHasDisabledChangeLabel}
           onHide={() => setCannotChangeModalShown(false)}
