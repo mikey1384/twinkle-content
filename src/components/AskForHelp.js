7781d31bee0fda539ba0bed19eb63ba25eedb9a4
mikey1384 <mikey1384@gmail.com>
2022-01-07 22:45:04 +0900

    add AskForHelp component

---
+++ src/components/AskForHelp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AskForHelp() {
+  return (
+    <div style={{ fontSize: '1.7rem' }}>
+      <span>{`We need your email address in order for us to make sure you are the owner of this account. `}</span>
+      Ask your Twinkle teacher for help. If you no longer attend Twinkle, send
+      an email to mikey at <b>mikey@twin-kle.com</b> for help
+    </div>
+  );
+}
