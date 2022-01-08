f038d0053563dff926959abbc686bff3daa40311
mikey1384 <mikey1384@gmail.com>
2022-01-08 12:19:00 +0900

    Change get help message to account for case where user is already logged in

--- src/components/AskForHelp.js
+++ src/components/AskForHelp.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useMyState } from 'helpers/hooks';
 
 export default function AskForHelp() {
+  const { userId } = useMyState();
   return (
     <div style={{ fontSize: '1.7rem' }}>
-      <span>{`We need your email address in order for us to make sure you are the owner of this account. `}</span>
-      Ask your Twinkle teacher for help. If you no longer attend Twinkle, send
-      an email to mikey at <b>mikey@twin-kle.com</b> for help
+      <span>{`We need your email address in order for us to make sure you are the owner of this account. `}</span>{' '}
+      Ask your Twinkle teacher for help. If you no longer attend Twinkle,{' '}
+      {userId ? (
+        <>
+          <b>send a chat message to mikey</b> or email him at{' '}
+          <b>mikey@twin-kle.com</b> for help
+        </>
+      ) : (
+        <>
+          send an email to mikey at <b>mikey@twin-kle.com</b> for help
+        </>
+      )}
     </div>
   );
 }
