f070206df558bdddded30fcec4cb224579fe6731
mikey1384 <mikey1384@gmail.com>
2022-01-10 21:54:13 +0900

    add emojis.png

--- src/containers/Chat/Message/ReactionButton/index.js
+++ src/containers/Chat/Message/ReactionButton/index.js
@@ -23,3 +23,9 @@ export default function ReactionButton({ style }) {
     </Button>
   );
 }
+
+/*
+
+<div class="emoji-list-wrapper"><div class="reaction-emoji-list "><div class="reaction-emoji-icon"><span class="emoji-sizer emoji-outer " style="background: url(&quot;assets/emoji.1e7786c93c8a0c1773f165e2de2fd129.png?v=20180604&quot;) 84% 82.5% / 5100%; -webkit-user-drag: none; margin: -1px; position: relative; top: 2px;">/-strong</span></div><div class="reaction-emoji-icon"><span class="emoji-sizer emoji-outer " style="background: url(&quot;assets/emoji.1e7786c93c8a0c1773f165e2de2fd129.png?v=20180604&quot;) 84% 72.5% / 5100%; -webkit-user-drag: none; margin: -1px; position: relative; top: 2px;">/-heart</span></div><div class="reaction-emoji-icon"><span class="emoji-sizer emoji-outer " style="background: url(&quot;assets/emoji.1e7786c93c8a0c1773f165e2de2fd129.png?v=20180604&quot;) 82% 7.5% / 5100%; -webkit-user-drag: none; margin: -1px; position: relative; top: 2px;">:&gt;</span></div><div class="reaction-emoji-icon"><span class="emoji-sizer emoji-outer " style="background: url(&quot;assets/emoji.1e7786c93c8a0c1773f165e2de2fd129.png?v=20180604&quot;) 84% 20% / 5100%; -webkit-user-drag: none; margin: -1px; position: relative; top: 2px;">:o</span></div><div class="reaction-emoji-icon"><span class="emoji-sizer emoji-outer " style="background: url(&quot;assets/emoji.1e7786c93c8a0c1773f165e2de2fd129.png?v=20180604&quot;) 84% 2.5% / 5100%; -webkit-user-drag: none; margin: -1px; position: relative; top: 2px;">:-((</span></div><div class="reaction-emoji-icon"><span class="emoji-sizer emoji-outer " style="background: url(&quot;assets/emoji.1e7786c93c8a0c1773f165e2de2fd129.png?v=20180604&quot;) 84% 5% / 5100%; -webkit-user-drag: none; margin: -1px; position: relative; top: 2px;">:-h</span></div><i class="fa fa-close clear-react"></i></div></div>
+
+*/
