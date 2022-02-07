f8b01c558b5d3867749392a5d7d225b32040bff6
mikey1384 <mikey1384@gmail.com>
2022-02-07 14:45:15 +0900

    improve notification loading speed

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,6 +1,6 @@
 import localize from 'constants/localize';
 
-export const clientVersion = '1.5.93';
+export const clientVersion = '1.5.94';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
