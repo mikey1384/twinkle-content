1e3859cf6a5ec516592739728639b01549d3cda8
mikey1384 <mikey1384@gmail.com>
2022-02-07 20:33:47 +0900

    change Earn XP section to make space for Rank history

--- src/containers/Home/Earn/Styles.js
+++ src/containers/Home/Earn/Styles.js
@@ -13,7 +13,6 @@ export const panel = css`
   }
   @media (max-width: ${mobileMaxWidth}) {
     border-radius: 0;
-    border-top: 0;
     border-left: 0;
     border-right: 0;
   }
--- src/containers/Home/Earn/index.js
+++ src/containers/Home/Earn/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ErrorBoundary from 'components/ErrorBoundary';
 import HighXPSubjects from './HighXPSubjects';
+import { css } from '@emotion/css';
+import { mobileMaxWidth } from 'constants/css';
 
 export default function Earn() {
   return (
     <ErrorBoundary>
-      <HighXPSubjects />
+      <section
+        className={css`
+          @media (max-width: ${mobileMaxWidth}) {
+            padding-top: 1rem;
+            > h2 {
+              padding-left: 1rem;
+            }
+          }
+        `}
+      >
+        <h2>Earn XP</h2>
+        <div style={{ marginTop: '1.3rem' }}>
+          <HighXPSubjects />
+        </div>
+      </section>
     </ErrorBoundary>
   );
 }
