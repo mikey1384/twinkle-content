05c6dc137609a59f4a4d90f645efdbc225918d17
mikey1384 <mikey1384@gmail.com>
2022-03-18 17:05:19 +0700

    make background orange if letter present

--- src/containers/Chat/Modals/WordleModal/Grid/Cell.js
+++ src/containers/Chat/Modals/WordleModal/Grid/Cell.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { Color } from 'constants/css';
 import { REVEAL_TIME_MS } from '../constants/settings';
 
 Cell.propTypes = {
+  status: PropTypes.string,
   value: PropTypes.string,
   position: PropTypes.number
 };
 
-export default function Cell({ value, position = 0 }) {
+export default function Cell({ status, value, position = 0 }) {
   const animationDelay = `${position * REVEAL_TIME_MS}ms`;
+  const borderColor = useMemo(() => {
+    if (status === 'present') {
+      return Color.orange();
+    }
+    return 'black';
+  }, [status]);
+  const backgroundColor = useMemo(() => {
+    if (status === 'present') {
+      return Color.orange();
+    }
+    return null;
+  }, [status]);
 
   return (
     <div
       style={{
         width: '3.5rem',
         height: '3.5rem',
-        border: '1px solid black',
+        border: `1px solid ${borderColor}`,
         marginRight: '0.5rem',
+        backgroundColor,
         animationDelay
       }}
     >
