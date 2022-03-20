f70b09625d5fe4d59b7465faebadf69fbf90fd08
mikey1384 <mikey1384@gmail.com>
2022-03-20 19:25:36 +0700

    more css refactoring

--- src/containers/Chat/Modals/WordleModal/Modals/StatsModal/Progress.js
+++ src/containers/Chat/Modals/WordleModal/Modals/StatsModal/Progress.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Color } from 'constants/css';
 
 Progress.propTypes = {
+  currentDayStatRow: PropTypes.bool,
   index: PropTypes.number,
   size: PropTypes.number,
   label: PropTypes.string
 };
-export default function Progress({ index, size, label }) {
+export default function Progress({ currentDayStatRow, index, size, label }) {
   return (
     <div style={{ display: 'flex', textAlign: 'left' }}>
       <div
@@ -19,7 +21,15 @@ export default function Progress({ index, size, label }) {
         {index + 1}
       </div>
       <div style={{ width: '100%', marginLeft: '0.5rem' }}>
-        <div style={{ width: `${8 + size}%` }}>{label}</div>
+        <div
+          style={{
+            width: `${8 + size}%`,
+            textAlign: 'center',
+            background: currentDayStatRow ? Color.blue() : Color.gray()
+          }}
+        >
+          {label}
+        </div>
       </div>
     </div>
   );
