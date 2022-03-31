52e38d05f744c85bd1705ae2b892c94224b5698e
mikey1384 <mikey1384@gmail.com>
2022-04-01 06:56:11 +0700

    month for montly ranking follow gmt time

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,6 +1,6 @@
 import localize from 'constants/localize';
 
-export const clientVersion = '1.6.4';
+export const clientVersion = '1.6.5';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
