bb0d51e188a14f3951b71e487f7da850d92df866
mikey1384 <mikey1384@gmail.com>
2022-05-19 18:58:59 +0700

    add borders to playlist page

--- src/containers/PlaylistPage/Content.js
+++ src/containers/PlaylistPage/Content.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Playlist from 'components/Playlist';
 import { useParams } from 'react-router-dom';
+import { css } from '@emotion/css';
+import { Color, mobileMaxWidth } from 'constants/css';
 
 export default function Content() {
   const { contentId } = useParams();
@@ -12,6 +14,14 @@ export default function Content() {
         background,
         padding: '1rem'
       }}
+      className={css`
+        border: 1px solid ${Color.borderGray()};
+        @media (max-width: ${mobileMaxWidth}) {
+          border-top: none;
+          border-left: none;
+          border-right: none;
+        }
+      `}
     >
       {title && <p style={{ fontSize: '3rem', fontWeight: 'bold' }}>{title}</p>}
       <div style={{ marginTop: '1rem' }}>
