620e7578d533ca6fad249127068adc4c650917ac
mikey1384 <mikey1384@gmail.com>
2022-05-26 09:24:18 +0700

    apply video modal for file attachment inside chat replies

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -1,6 +1,6 @@
 import localize from 'constants/localize';
 
-export const clientVersion = '1.6.23';
+export const clientVersion = '1.6.24';
 export const defaultChatSubject = 'Welcome!';
 export const defaultContentState = {
   isEditing: false,
