48062fa1db14977c102da20884e69017fd18f848
mikey1384 <mikey1384@gmail.com>
2022-05-31 14:03:31 +0700

    refactor Likers component

---
+++ src/components/Likers/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ErrorBoundary from 'components/ErrorBoundary';
+import InnerContent from './InnerContent';
+
+Likers.propTypes = {
+  className: PropTypes.string,
+  defaultText: PropTypes.string,
+  likes: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      username: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  onLinkClick: PropTypes.func.isRequired,
+  style: PropTypes.object,
+  target: PropTypes.string,
+  wordBreakEnabled: PropTypes.bool,
+  userId: PropTypes.number
+};
+
+export default function Likers({
+  likes,
+  target,
+  userId,
+  onLinkClick,
+  style = {},
+  className,
+  defaultText,
+  wordBreakEnabled
+}) {
+  return (
+    <ErrorBoundary>
+      <div style={style} className={className}>
+        <InnerContent
+          defaultText={defaultText}
+          likes={likes}
+          onLinkClick={onLinkClick}
+          target={target}
+          userId={userId}
+          wordBreakEnabled={wordBreakEnabled}
+        />
+      </div>
+    </ErrorBoundary>
+  );
+}
