6c66a7fc0dbcb749304fb5b64b3f4674d289baf9
mikey1384 <mikey1384@gmail.com>
2022-06-11 20:12:35 +0700

    finish css

--- src/containers/Chat/Message/TargetMessage/WordleResult.js
+++ src/containers/Chat/Message/TargetMessage/WordleResult.js
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import ErrorBoundary from 'components/ErrorBoundary';
 import { css } from '@emotion/css';
-import { Color, mobileMaxWidth } from 'constants/css';
+import { borderRadius, Color, mobileMaxWidth } from 'constants/css';
 import { useMyState, useWordleLabels } from 'helpers/hooks';
 import { unix } from 'moment';
 
@@ -20,12 +20,14 @@ export default function WordleResult({
   wordleResult
 }) {
   const { userId: myId } = useMyState();
-  const { resultLabel, solutionLabel } = useWordleLabels({
-    ...wordleResult,
-    username,
-    userId,
-    myId
-  });
+  const { numGuesses } = wordleResult;
+  const { guessLabel, guessLabelColor, resultLabel, solutionLabel } =
+    useWordleLabels({
+      ...wordleResult,
+      username,
+      userId,
+      myId
+    });
   const displayedTimeStamp = useMemo(
     () => unix(timeStamp).format('lll'),
     [timeStamp]
@@ -40,43 +42,86 @@ export default function WordleResult({
         style={{
           width: '100%',
           height: '100%',
-          background: Color.darkBlueGray(),
-          color: '#fff',
-          marginBottom: '1.5rem',
-          position: 'relative'
+          marginTop: '0.5rem',
+          marginBottom: '1rem',
+          padding: '1.5rem',
+          border: `1px solid ${Color.lightGray()}`,
+          background: Color.wellGray(),
+          display: 'flex',
+          alignItems: 'center',
+          borderRadius
         }}
       >
         <div
-          className={css`
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-            height: 100%;
-            padding: 2rem 1rem;
-            @media (max-width: ${mobileMaxWidth}) {
-              font-size: 1.1rem;
-            }
-          `}
-        >
-          <div style={{ textAlign: 'center' }}>{resultLabel}</div>
-          <p style={{ marginTop: '0.5rem' }}>{solutionLabel}</p>
-        </div>
-        <div
           style={{
-            position: 'absolute',
-            bottom: '5px',
-            right: '8px'
+            width: '100%',
+            background: Color.darkBlueGray(),
+            color: '#fff',
+            position: 'relative'
           }}
-          className={css`
-            font-size: 0.8rem;
-            @media (max-width: ${mobileMaxWidth}) {
-              font-size: 0.6rem;
-            }
-          `}
         >
-          {displayedTimeStamp}
+          <div
+            className={css`
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+              width: 100%;
+              height: 100%;
+              padding: 2rem 1rem;
+              font-size: 1.5rem;
+              @media (max-width: ${mobileMaxWidth}) {
+                font-size: 1rem;
+              }
+            `}
+          >
+            {guessLabel && (
+              <p
+                style={{
+                  marginBottom: '0.5rem',
+                  color: guessLabelColor,
+                  fontWeight: 'bold'
+                }}
+                className={css`
+                  font-size: ${numGuesses === 1
+                    ? '2.9rem'
+                    : numGuesses === 2
+                    ? '2.4rem'
+                    : numGuesses === 3
+                    ? '2.1rem'
+                    : '1.9rem'};
+                  @media (max-width: ${mobileMaxWidth}) {
+                    font-size: ${numGuesses === 1
+                      ? '2.1rem'
+                      : numGuesses === 2
+                      ? '1.8rem'
+                      : numGuesses === 3
+                      ? '1.5rem'
+                      : '1.3rem'};
+                  }
+                `}
+              >
+                {guessLabel}
+              </p>
+            )}
+            <div style={{ textAlign: 'center' }}>{resultLabel}</div>
+            <p style={{ marginTop: '0.5rem' }}>{solutionLabel}</p>
+          </div>
+          <div
+            style={{
+              position: 'absolute',
+              bottom: '5px',
+              right: '8px'
+            }}
+            className={css`
+              font-size: 0.8rem;
+              @media (max-width: ${mobileMaxWidth}) {
+                font-size: 0.6rem;
+              }
+            `}
+          >
+            {displayedTimeStamp}
+          </div>
         </div>
       </div>
     </ErrorBoundary>
