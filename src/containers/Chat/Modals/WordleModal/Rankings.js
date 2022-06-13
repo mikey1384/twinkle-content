5b9d17ea58d96330d0aeab04b54d8749b0a0c69a
mikey1384 <mikey1384@gmail.com>
2022-06-13 20:38:46 +0700

    add RoundList

--- src/containers/Chat/Modals/WordleModal/Rankings.js
+++ src/containers/Chat/Modals/WordleModal/Rankings.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import FilterBar from 'components/FilterBar';
 import localize from 'constants/localize';
+import RoundList from 'components/RoundList';
+import RankingsListItem from 'components/RankingsListItem';
+import { useMyState } from 'helpers/hooks';
 
 const myRankingLabel = localize('myRanking');
 const top30Label = localize('top30');
 
 Rankings.propTypes = {
   onSetRankingsTab: PropTypes.func.isRequired,
-  rankingsTab: PropTypes.string.isRequired
+  rankingsTab: PropTypes.string.isRequired,
+  allRanks: PropTypes.array,
+  top30s: PropTypes.array
 };
 
-export default function Rankings({ rankingsTab, onSetRankingsTab }) {
+export default function Rankings({
+  allRanks = [],
+  top30s = [],
+  rankingsTab,
+  onSetRankingsTab
+}) {
+  const { userId: myId } = useMyState();
+  const users = useMemo(
+    () => (rankingsTab === 'all' ? allRanks : top30s),
+    [allRanks, rankingsTab, top30s]
+  );
+
   return (
     <div style={{ width: '35rem' }}>
       <FilterBar
@@ -35,6 +51,11 @@ export default function Rankings({ rankingsTab, onSetRankingsTab }) {
           {top30Label}
         </nav>
       </FilterBar>
+      <RoundList style={{ marginTop: 0 }}>
+        {users.map((user) => (
+          <RankingsListItem key={user.id} user={user} myId={myId} />
+        ))}
+      </RoundList>
     </div>
   );
 }
